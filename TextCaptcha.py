5b8d7f6769e8820e2b423bc22abda69a34009919
Andrei <drang.andray@gmail.com>
2017-08-06 01:46:36 +0300

    Небольшие правки пробелов/табов.
    Всё приведено к табам.

--- TextCaptcha.py
+++ TextCaptcha.py
@@ -13,22 +13,22 @@ class TextCaptcha:
 	def handle_captcha(self):
 		# Создаём пайлоад, вводим ключ от сайта, выбираем метод ПОСТ и ждём ответа. в JSON-формате
 		payload = {"key": self.RECAPTCHA_KEY,
-	               "method": "textcaptcha",
-	               "json": 1,
-	               "textinstructions": self.captcha_text}
+					"method": "textcaptcha",
+					"json": 1,
+					"textinstructions": self.captcha_text}
 		# Отправляем на рукапча текст капчи и ждём ответа
 		#  в результате получаем JSON ответ с номером решаемой капчи
 		captcha_id = (requests.request('POST',
-		                               "http://rucaptcha.com/in.php",
-		                               data=payload).json())['request']
+										"http://rucaptcha.com/in.php",
+										data=payload).json())['request']
 		# Ожидаем решения капчи
 		time.sleep(self.sleep_time)
 		while True:
 			# отправляем запрос на результат решения капчи, если ещё капча не решена - ожидаем 5 сек
-	        # если всё ок - идём дальше
+			#  если всё ок - идём дальше
 			captcha_response = requests.request('GET',
-	                                            "http://rucaptcha.com/res.php?key={0}&action=get&id={1}&json=1"
-	                                            .format(self.RECAPTCHA_KEY, captcha_id))
+												"http://rucaptcha.com/res.php?key={0}&action=get&id={1}&json=1"
+												.format(self.RECAPTCHA_KEY, captcha_id))
 			if captcha_response.json()["request"] == 'CAPCHA_NOT_READY':
 				time.sleep(self.sleep_time)
 			else:
