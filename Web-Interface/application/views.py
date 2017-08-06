638b409a2abd133a692be79d75ad84413a29dd4e
Andrei <drang.andray@gmail.com>
2017-08-06 18:33:45 +0300

    Добавлена рандомность отображения обычнйо капчи

--- Web-Interface/application/views.py
+++ Web-Interface/application/views.py
@@ -1,19 +1,26 @@
 from flask import render_template, redirect, request, url_for, session
 from application import app
 import os
+import random
 
 
 @app.route('/')
 @app.route('/index/')
 def index():
+
 	payload = {
-		"common_captcha_source": "/application/templates/common_image_example/3.jpg"
+		"common_captcha_source": common_captcha_source()
 	}
 
 	return render_template('base.html', doc = '/index.html', payload=payload)
 
+
+# Функция которая возвращает рандомное изображение обычной капчи
 def common_captcha_source():
-	pass
+	# Получаем список всех изображений и возвращаем рандомную картинку
+	images_list = os.listdir('application/static/image/common_image_example/')
+	return random.choice(images_list)
+	
 
 # ERRORS
 @app.errorhandler(404)
