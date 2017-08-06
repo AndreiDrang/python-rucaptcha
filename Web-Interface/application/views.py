20e8c23ee1219abb33a4aac12776641957d6a76d
Andrei <drang.andray@gmail.com>
2017-08-06 18:20:23 +0300

    Изменена разметка, начато размещение капчей
    Размещена рекапча нова, невидимая рекапча. В процессе размещения капча текстовая и изображением(большим/малым)

--- Web-Interface/application/views.py
+++ Web-Interface/application/views.py
@@ -6,5 +6,20 @@ import os
 @app.route('/')
 @app.route('/index/')
 def index():
-    return render_template('base.html', doc = '/index.html', menu = True)
+	payload = {
+		"common_captcha_source": "/application/templates/common_image_example/3.jpg"
+	}
 
+	return render_template('base.html', doc = '/index.html', payload=payload)
+
+def common_captcha_source():
+	pass
+
+# ERRORS
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('base.html', doc = 'mistakes/404.html')
+
+@app.errorhandler(500)
+def page_not_found(e):
+	return render_template('base.html', doc = 'mistakes/500.html')
