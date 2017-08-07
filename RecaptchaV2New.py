9c29e57e30f414be1aa56c6424c512eaff840634
redV0ID <dogeofsiberia@gmail.com>
2017-08-08 06:40:32 +0700

    Готово к тестам и правкам

--- RecaptchaV2New.py
+++ RecaptchaV2New.py
@@ -19,17 +19,10 @@ class ReCaptcha:
 		
 		answer = requests.request('GET', captcha_download)
 		print(answer.json())
-		#пейлоад с ключем сайта, методом, и ответом в json формате
-		payload = {"key": self.RECAPTCHA_KEY,
-					"method": "post",
-					"json": 1}
 
 		#получаем ID капчи
-		captcha_id = {(requests.request('POST',
-										"http://rucaptcha.com/in.php",
-										data=payload).json())['request']}
+		captcha_id = answer.json()['request']
 		print(captcha_id)
-		answer = 'http://rucaptcha.com/res.php?key={0}&action=get&id={1}&json=1'.format(self.RECAPTCHA_KEY, captcha_id)
 		
 		# Ожидаем решения капчи
 		time.sleep(self.sleep_time * 4)
