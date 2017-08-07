8883c5e2d92f28b31c4a86c94edfccbd4434b664
redV0ID <dogeofsiberia@gmail.com>
2017-08-08 04:02:25 +0700

    Добавлен captcha_handler не тестированный

--- RecaptchaV2New.py
+++ RecaptchaV2New.py
@@ -1,27 +1,5 @@
 import requests
 import time
-# Checking captcha
-def captcha():
-    captcha_download = 'http://rucaptcha.com/in.php?key={0}&method=userrecaptcha&googlekey={1}'\
-        .format(recaptcha_key,site_key)
-    # then i have answer like: OK|CAPTCHA_ID
-    # checking captcha
-    http = urllib3.PoolManager()
-    answer = http.request('GET', captcha_download)
-    # get captcha ID
-    captcha_id = (str(answer.data).split('|'))[1]
-    answer = 'http://rucaptcha.com/res.php?key={0}&action=get&id={1}'.format(recaptcha_key, captcha_id)
-    # get answer on captcha
-    time.sleep(13)
-    while True:
-        captcha_response = requests.request('GET', answer)
-        if str(captcha_response.content) == 'CAPCHA_NOT_READY':
-            time.sleep( 5 )
-        else:
-            answer = (str(captcha_response.content).split('|'))[1]
-            break
-    return answer
-
 
 class ReCaptcha:
 	def __init__(self, recaptcha_api, sleep_time=5):
@@ -31,5 +9,23 @@ class ReCaptcha:
         self.sleep_time = sleep_time
 
     #Работа с капчей
-	def handle_captcha(self):
-		pass
+	def handle_captcha(self, ):
+		captcha_download = 'http://rucaptcha.com/in.php?key={0}&method=userrecaptcha&googlekey={1}'\
+        	.format(recaptcha_key,site_key)
+    	# then i have answer like: OK|CAPTCHA_ID
+    	# checking captcha
+    	http = urllib3.PoolManager()
+    	answer = http.request('GET', captcha_download)
+    	# get captcha ID
+    	captcha_id = (str(answer.data).split('|'))[1]
+    	answer = 'http://rucaptcha.com/res.php?key={0}&action=get&id={1}'.format(recaptcha_key, captcha_id)
+    	# get answer on captcha
+    	time.sleep(13)
+    	while True:
+        	captcha_response = requests.request('GET', answer)
+        	if str(captcha_response.content) == 'CAPCHA_NOT_READY':
+            	time.sleep( 5 )
+        	else:
+            	answer = (str(captcha_response.content).split('|'))[1]
+            	break
+    	return answer
