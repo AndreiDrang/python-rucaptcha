9b01150b25f48cc7b0e4a5483525581a9419ab31
redV0ID <dogeofsiberia@gmail.com>
2017-08-08 04:58:01 +0700

    still not working but im trying!

--- RecaptchaV2New.py
+++ RecaptchaV2New.py
@@ -1,23 +1,25 @@
 import requests
 import time
+import urllib3
+#site_key = "6Lf77CsUAAAAALLFD1wIhbfQRD07VxhvPbyQFaQJ"
 
 class ReCaptcha:
 	def __init__(self, recaptcha_api, sleep_time=5):
-		self.url_request = "http://2captcha.com/in.php"
-		self.url_response = "http://2captcha.com/res.php"
 		self.RECAPTCHA_KEY = recaptcha_api
 		self.sleep_time = sleep_time
 
 	#Работа с капчей
-	def handle_captcha(self, RECAPTCHA_KEY, site_key):
+	#тестовый ключ сайта
+	def captcha_handler(self, site_key):
 		captcha_download = 'http://rucaptcha.com/in.php?key={0}&method=userrecaptcha&googlekey={1}'\
-			.format(RECAPTCHA_KEY, site_key)
+			.format(self.RECAPTCHA_KEY, site_key)
 		# проверяем капчу
 		http = urllib3.PoolManager()
 		answer = http.request('GET', captcha_download)
+		print(answer)
 		#получаем ID капчи
-		captcha_id = (str(answer.data).split('|'))[1]
-		answer = 'http://rucaptcha.com/res.php?key={0}&action=get&id={1}'.format(RECAPTCHA_KEY, captcha_id)
+		captcha_id = (str(answer.data).split('|'))
+		answer = 'http://rucaptcha.com/res.php?key={0}&action=get&id={1}'.format(self.RECAPTCHA_KEY, captcha_id)
 		# Ожидаем решения капчи
 		time.sleep(self.sleep_time)
 		while True:
@@ -26,3 +28,6 @@ class ReCaptcha:
 				time.sleep(self.sleep_time)
 			else:
 				return str(captcha_response.content).split('|')[1]
+
+
+
