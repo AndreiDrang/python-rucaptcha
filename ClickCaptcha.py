47625a418b064629370aea9fc3bd5f2c7ef156c7
redV0ID <thousandkats@gmail.com>
2017-08-08 18:57:01 +0700

    Создание папки для картинок

--- ClickCaptcha.py
+++ ClickCaptcha.py
@@ -5,7 +5,14 @@ class ClickCaptcha:
 	def __init__(self, recaptcha_api, sleep_time=5):
 		self.RECAPTCHA_KEY = recaptcha_api
 		self.sleep_time = sleep_time
+		self.img_path = os.path.normpath('click_images')
 		#создать папку для сохранения картинок
+		
+		try:
+			if not os.path.exists(self.img_path):
+				os.mkdir(self.img_path)
+		except Exception as err:
+			print(err)
 
 
 	def captcha_handler(self):
