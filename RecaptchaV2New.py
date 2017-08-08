fcad9816d6e23cbafa112769eaeab1b07f3578bf
Andrei <drang.andray@gmail.com>
2017-08-08 10:26:23 +0300

    В веб-интерфейс добавлены обработчики капчи. Небольшие изменения в хэндлере рекапчи 2 в библиотеке

--- RecaptchaV2New.py
+++ RecaptchaV2New.py
@@ -1,7 +1,9 @@
 import requests
 import time
 import urllib3
-#site_key = "6Lf77CsUAAAAALLFD1wIhbfQRD07VxhvPbyQFaQJ"
+
+
+# site_key = "6Lf77CsUAAAAALLFD1wIhbfQRD07VxhvPbyQFaQJ"
 
 class ReCaptcha:
 	def __init__(self, recaptcha_api, sleep_time=5):
@@ -9,29 +11,26 @@ class ReCaptcha:
 		self.url_response = "http://2captcha.com/res.php"
 		self.RECAPTCHA_KEY = recaptcha_api
 		self.sleep_time = sleep_time
-
-	#Работа с капчей
-	#тестовый ключ сайта
+	
+	# Работа с капчей
+	# тестовый ключ сайта
 	def captcha_handler(self, site_key="6Lf77CsUAAAAALLFD1wIhbfQRD07VxhvPbyQFaQJ", page_url='http://127.0.0.1:5000/'):
 		
-		captcha_download = 'http://rucaptcha.com/in.php?key={0}&method=userrecaptcha&googlekey={1}&pageurl={2}&json=1'\
+		captcha_download = 'http://rucaptcha.com/in.php?key={0}&method=userrecaptcha&googlekey={1}&pageurl={2}&json=1' \
 			.format(self.RECAPTCHA_KEY, site_key, page_url)
 		
-		#получаем ID капчи
+		# получаем ID капчи
 		answer = requests.request('GET', captcha_download)
 		captcha_id = answer.json()['request']
 		
 		# Ожидаем решения капчи 20 секунд
 		time.sleep(self.sleep_time * 4)
-
+		
 		while True:
-			#отправляем запрос на результат решения капчи, если не решена ожидаем 6 секунд
+			# отправляем запрос на результат решения капчи, если не решена ожидаем 6 секунд
 			captcha_response = requests.request('GET', "http://rucaptcha.com/res.php?key={0}&action=get&id={1}&json=1"
-											.format(self.RECAPTCHA_KEY, captcha_id))
+			                                    .format(self.RECAPTCHA_KEY, captcha_id))
 			if captcha_response.json()["request"] == 'CAPCHA_NOT_READY':
 				time.sleep(6)
 			else:
 				return captcha_response.json()["request"]
-
-
-
--- Web-Interface/application/views.py
+++ Web-Interface/application/views.py
@@ -1,7 +1,10 @@
-from flask import render_template, redirect, request, url_for, session
+from flask import Response, render_template, redirect, request, url_for, session
 from application import app
 import os
 import random
+import json
+import requests
+
 from .dbconnect import Database
 
 
@@ -15,12 +18,15 @@ def index():
 	# Обработка форм
 	if request.method == 'POST':
 		# Скрытие записки
-		if "common_captcha_btn" in request.method:
-			print(request.form["common_captcha_src"])
-			print(request.form["common_captcha_answer"])
-		return render_template('base.html', doc='/index.html', payload=payload)
-	else:
-		return render_template('base.html', doc = '/index.html', payload=payload)
+		if "common_captcha_btn" in request.form:
+			# Проверяем капчу и ответ на соответсвие
+			return common_captcha_answer(request.form["common_captcha_src"],
+			                               request.form["common_captcha_answer"])
+		elif "recaptcha_new_btn" in request.form:
+			return recaptcha_v2_new_answer(request.form["g-recaptcha-response"])
+			
+
+	return render_template('base.html', doc = '/index.html', payload=payload)
 
 
 # Функция которая возвращает рандомное изображение обычной капчи
@@ -29,6 +35,60 @@ def common_captcha_source():
 	images_list = os.listdir('application/static/image/common_image_example/')
 	return random.choice(images_list)
 	
+'''
+API response
+'''
+# Обработчик капчи изображением
+def common_captcha_answer(captcha_name, user_answer):
+	if user_answer == captcha_name.split(".")[0]:
+		data = {'request': 'OK'}
+		
+		js = json.dumps(data)
+		
+		response = Response(js, status=200, mimetype='application/json')
+		response.headers['Link'] = 'http://127.0.0.1:5000'
+		
+		return response
+	else:
+		data = {'request': 'FAIL'}
+		
+		js = json.dumps(data)
+		
+		response = Response(js, status=200, mimetype='application/json')
+		response.headers['Link'] = 'http://127.0.0.1:5000'
+		
+		return response
+# Обработчик новой рекапчи версии 2
+def recaptcha_v2_new_answer(g_recaptcha_response):
+	# Проверяем решил ли юзер капчу
+	payload = {
+				"secret": "6Lf77CsUAAAAAMJ1yJWbEG1VyVYKIQZWVQJRg25t",
+				"response": g_recaptcha_response,
+			}
+	# Отсылаем запрос на правильность капчи
+	captcha_answer = requests.post("https://www.google.com/recaptcha/api/siteverify", data=payload)
+
+	if captcha_answer.json()["success"]=="true":
+		data = {'request': 'OK'}
+		
+		js = json.dumps(data)
+		
+		response = Response(js, status=200, mimetype='application/json')
+		response.headers['Link'] = 'http://127.0.0.1:5000'
+		
+		return response
+	else:
+		data = {'request': 'FAIL'}
+		# Если есть ошибки - высылаем и их
+		if "error-codes" in captcha_answer.json() and captcha_answer.json()["error-codes"]!=[]:
+			data.update({"recaptcha_error": captcha_answer.json()["error-codes"]})
+		
+		js = json.dumps(data)
+		
+		response = Response(js, status=200, mimetype='application/json')
+		response.headers['Link'] = 'http://127.0.0.1:5000'
+		
+		return response
 
 # ERRORS
 @app.errorhandler(404)
