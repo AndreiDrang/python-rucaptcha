733aa16a05c47736c6543e3f8982bf02a8d6f249
redV0ID <dogeofsiberia@gmail.com>
2017-08-08 17:51:41 +0700

    Добавил скелет класса, TODO-комментарии

---
+++ ClickCaptcha.py
@@ -0,0 +1,15 @@
+class ClickCaptcha:
+	
+	def __init__(self, recaptcha_api, sleep_time=5):
+		self.RECAPTCHA_KEY = recaptcha_api
+		self.sleep_time = sleep_time
+		#создать папку для сохранения картинок
+
+
+	def captcha_handler(self):
+		pass
+		#получить изображение и инструкцию
+		#отправить запрос POST к http://rucaptcha.com/in.php c параметром coordiantescaptcha=1
+		#получить ID капчи или код ошибки
+		#ждем 5 секунд
+		#отправляем запрос GET к http://rucaptcha.com/res.ph чтобы получить результат
