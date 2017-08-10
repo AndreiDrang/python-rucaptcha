ab21fca31e3809ecc149b6001d6d3a941dc23ead
redV0ID <thousandkats@gmail.com>
2017-08-10 17:45:46 +0700

    Дописана часть с сохранением картинки

--- ClickCaptcha.py
+++ ClickCaptcha.py
@@ -1,5 +1,6 @@
 import requests
 import os
+
 class ClickCaptcha:
 	
 	def __init__(self, recaptcha_api, sleep_time=5):
@@ -11,13 +12,24 @@ class ClickCaptcha:
 		try:
 			if not os.path.exists(self.img_path):
 				os.mkdir(self.img_path)
+			if not os.path.exists(".cache"):
+				os.mkdir(".cache")
 		except Exception as err:
 			print(err)
 
 
-	def captcha_handler(self):
-		pass
+	def captcha_handler(self, captcha_link):
 		#получить изображение и инструкцию
+		
+		#сохранение изображения
+		image_hash = hashlib.sha224(captcha_link.encode('utf-8')).hexdigest()
+		cache = httplib2.Http(".cache")
+		response, content = cache.request(captcha_link)
+		out = open(os.path.join(self.img_path, 'im-{0}.jpg'.format(image_hash)), 'wb')
+		out.write(content)
+		out.close()
+
+
 		#отправить запрос POST к http://rucaptcha.com/in.php c параметром coordiantescaptcha=1
 		#получить ID капчи или код ошибки
 		#ждем 5 секунд
