349bf8ed49d6beea57051d176e4ea46ed5457e8c
redV0ID <thousandkats@gmail.com>
2017-08-10 20:29:30 +0700

    Написан captcha_handler, __del__

--- ClickCaptcha.py
+++ ClickCaptcha.py
@@ -1,5 +1,9 @@
 import requests
-import os
+import os, shutil
+import hashlib
+import httplib2
+import time
+
 
 class ClickCaptcha:
 	
@@ -25,20 +29,43 @@ class ClickCaptcha:
 		image_hash = hashlib.sha224(captcha_link.encode('utf-8')).hexdigest()
 		cache = httplib2.Http(".cache")
 		response, content = cache.request(captcha_link)
-		out = open(os.path.join(self.img_path, 'im-{0}.jpg'.format(image_hash)), 'wb')
+		out = open(os.path.join(self.img_path, 'img-{0}.jpg'.format(image_hash)), 'wb')
 		out.write(content)
 		out.close()
 
 		with open(os.path.join(self.img_path, "img-{0}.jpg".format(image_hash)), 'rb') as captcha_image:
 			files = {"file": captcha_image}
 			#отправить запрос POST к http://rucaptcha.com/in.php c параметром coordiantescaptcha=1
-			payload = {"key": self.RECAPTCHA_KEY, "method": "post", "coordinatecaptcha": 1, "json": 1}
+			payload = {"key": self.RECAPTCHA_KEY, 
+						"method": "post", 
+						"textinstructions": "!instructions example!",
+						"coordinatecaptcha": 1, 
+						"json": 1}
+			#получить ID капчи или код ошибки
 			captcha_id = (requests.request("POST",
 											"http://rucaptcha.com/in.php",
 											data=payload,
-											files=files).json()).["request"]
-
-		#получить ID капчи или код ошибки
+											files=files).json())["request"]
+			print(captcha_id)
+		#удаляем временные файлы и картинку
+		#os.remove(os.path.join(self.img_path, "img-{0}.jpg".format(image_hash)))
 		#ждем 5 секунд
-		#отправляем запрос GET к http://rucaptcha.com/res.ph чтобы получить результат
-		#если капча решена, сервер вернет координаты точек по которым нужно кликнуть
+		time.sleep(self.sleep_time * 4)
+		
+		while True:
+			#отправляем запрос на результат решения капчи
+			captcha_response = requests.request("GET",
+												"http://rucaptcha.com/res.php?key={0}&action=get&id={1}&json=1".
+												format(self.RECAPTCHA_KEY, captcha_id))
+			#если капча решена, сервер вернет координаты точек по которым нужно кликнуть
+			if captcha_response.json()["request"] == 'CAPTCHA_NOT_READY':
+				time.sleep(6)
+			else:
+				return captcha_response.json()["request"]
+
+
+	def __del__(self):
+		if os.path.exists("click_images"):
+			shutil.rmtree("click_images")
+		if os.path.exists(".cache"):
+			shutil.rmtree(".cache")
