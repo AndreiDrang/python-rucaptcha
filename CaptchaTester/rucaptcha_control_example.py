377345473cd041b618f3db9249115992a8e3b05c
Andrei <drang.andray@gmail.com>
2017-10-16 14:15:24 +0300

    Переработан модуль отвечающий за управление. Получение баланса и прочие дополнительные действия.

--- CaptchaTester/rucaptcha_control_example.py
+++ CaptchaTester/rucaptcha_control_example.py
@@ -7,20 +7,55 @@ from python_rucaptcha import RuCaptchaControl
 # Введите ключ от рукапчи из своего аккаунта
 RUCAPTCHA_KEY = ""
 
-# Пример получения актуального баланса средств на аккаунте
-answer = RuCaptchaControl.RuCaptchaControl(rucaptcha_key=RUCAPTCHA_KEY).get_balance()
+# пример получения информации о балансе
+answer = RuCaptchaControl.RuCaptchaControl(rucaptcha_key = RUCAPTCHA_KEY).additional_methods(action = 'getbalance')
 
-print("Your balance is: ", answer, " rub.")
+"""
+answer - это JSON строка с соответствующими полями:
+serverAnswer - ответ сервера,
+errorId - 0 - если всё хорошо, 1 - если есть ошибка,
+errorBody - тело ошибки, если есть.
+{
+    "serverAnswer": string,
+    "errorId": int, 1 or 0,
+    "errorBody": string
+}
+"""
+
+if answer['errorId'] == 0:
+    print("Your balance is: ", answer['serverAnswer'], " rub.")
+
+elif answer['errorId'] == 1:
+    # Тело ошибки, если есть
+    print(answer['errorBody'])
 
 
 # Пример отправки жалобы на неправильно решённую капчу под ID "666"
 wrong_captcha_id = 666
 
-answer = RuCaptchaControl.RuCaptchaControl(rucaptcha_key = RUCAPTCHA_KEY).complaint_on_result(reported_id = wrong_captcha_id)
+answer = RuCaptchaControl.RuCaptchaControl(rucaptcha_key = RUCAPTCHA_KEY).additional_methods(action = 'reportbad',
+                                                                                             id = wrong_captcha_id)
 
 # Если заявка принята
-if answer=='OK':
+if answer['errorId'] == 0:
     print("Заявка принята.")
+
 # Если возникла ошибка
-elif answer=='Error':
-    print(answer['err'])
+elif answer['errorId'] == 1:
+    print(answer['errorBody'])
+
+"""
+Возможны следующие варианты 'action':
+getbalance - получить ваш баланс
+get - получить ответы на множество капч с помощью одного запроса. Требует указания параметра ids.
+get2 - получить стоимость решения отправленной капчи и ответ на нее. Требует указания ID капчи в параметре id
+
+А так же дополнительные параметры
+ids	Строка	-	ID ваших капч, разделенные запятыми.
+id	Строка	-	ID вашей капчи.
+
+При action = 'reportbad', id - ID неверно решенной капчи и является обязательным параметром.
+
+Подробней обо всех возможных параметрах и их применении:
+https://rucaptcha.com/api-rucaptcha#complain
+"""
--- python_rucaptcha/RuCaptchaControl.py
+++ python_rucaptcha/RuCaptchaControl.py
@@ -1,5 +1,4 @@
 import requests
-import time
 
 from .errors import RuCaptchaError
 from .config import url_response
@@ -7,45 +6,46 @@ from .config import url_response
 
 class RuCaptchaControl:
     def __init__(self, rucaptcha_key):
-        self.RUCAPTCHA_KEY = rucaptcha_key
+        self.payload = {'key': rucaptcha_key,
+                        'json': 1,
+                        }
+        # результат возвращаемый методом *captcha_handler*
+        # в serverAnswer - ответ сервера на ваши действия,
+        # в errorId - 0 - если всё хорошо, 1 - если есть ошибка,
+        # в errorBody - тело ошибки, если есть
+        self.result = {"serverAnswer": None,
+                       "errorId": None,
+                       "errorBody": None}
 
-    def get_balance(self):
-        '''
-        Получение баланса аккаунта
-        :return: Возвращает актуальный баланс
-        '''
-        payload = {'key': self.RUCAPTCHA_KEY,
-                   'action': 'getbalance',
-                   'json': 1,
-                   }
-        # отправляем запрос на кол-во средств на аккаунте
-        answer = requests.post(url_response, data = payload)
+    def additional_methods(self, action, **kwargs):
 
-        if answer.json()['request'] == 'CAPCHA_NOT_READY':
-            time.sleep(5)
-        elif answer.json()["status"] == 0:
-            return RuCaptchaError(answer.json()["request"])
+        # Если переданы ещё параметры - вносим их в payload
+        if kwargs:
+            for key in kwargs:
+                self.payload.update({key: kwargs[key]})
+
+        self.payload.update({'action': action})
+
+        try:
+            # отправляем запрос на сервер с вашим запросом
+            answer = requests.post(url_response, data = self.payload)
+        except Exception as error:
+            self.result.update({'errorId': 1,
+                                'errorBody': error,
+                                }
+                               )
+            return self.result
+
+        if answer.json()["status"] == 0:
+            self.result.update({'errorId': 1,
+                                'errorBody': RuCaptchaError().errors(answer.json()["request"])
+                                }
+                               )
+            return self.result
+
         elif answer.json()["status"] == 1:
-            return answer.json()['request']
-
-    def complaint_on_result(self, reported_id):
-        '''
-        Позволяет отправить жалобуна неправильно решённую капчу.
-        :param reported_id: Отправляете ID капчи на которую нужно пожаловаться
-        :return: Возвращает результат действия
-        '''
-        payload = {'key': self.RUCAPTCHA_KEY,
-                   'action': 'reportbad',
-                   'id': reported_id,
-                   'json': 1,
-                   }
-        # отправляем запрос на репорт
-        answer = requests.post(url_response, data = payload)
-
-        if answer.json()['request'] == 'CAPCHA_NOT_READY':
-            time.sleep(5)
-        elif answer.json()["status"] == 0:
-            return RuCaptchaError(answer.json()["request"])
-        elif answer.json()["status"] == 1:
-            return answer.json()['request']
-
+            self.result.update({'errorId': 0,
+                                'serverAnswer': answer.json()['request']
+                                }
+                               )
+            return self.result
