b8b1f11731cb094baa921e66a1b25c82fb89e505
Andrei <drang.andray@gmail.com>
2018-09-16 00:34:00 +0300

    Обновление версии библиотеки.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'python-rucaptcha',
-    version = '2.1',
+    version = '2.1.1',
     author = 'AndreiDrang, redV0ID',
 
     packages = ['python_rucaptcha'],
