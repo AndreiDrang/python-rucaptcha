9380ceeaf5b8f66a9de26c73e91272267ca25a35
Andrei <drang.andray@gmail.com>
2018-10-29 22:07:58 +0300

    В пример клиента добавлена пример обработки поступивших сообщений.
    Добавлена возможность обработки всех новых сообщений по одному и выборка нужных(по ID задания), и дальнейшее их удаление/не удаление.
    Удалён старый способ вывода сообщений - всех подряд без возможности корректной обработки.

--- CaptchaTester/callback_examples/client.py
+++ CaptchaTester/callback_examples/client.py
@@ -1,6 +1,7 @@
 import asyncio
 import time
 import random
+import json
 
 import pika
 import aiohttp
@@ -10,29 +11,38 @@ import aiohttp
 YOUR_HOST_OR_IP:PORT
 """
 host = '85.255.8.26'
-port = 80
+port = 8001
 RUCAPTCHA_KEY = 'ba86e77f9007a106c2eb2d7436e7444060657442674'
+TASK_ID = '60657442675'
 
-def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+USERNAME = 'hardworker_1'
+PASSWORD = 'password'
 
-    channel.basic_ack(delivery_tag = method.delivery_tag)
-    channel.stop_consuming()
-def wait_queue_elements(queue_name: str):
+def wait_message(queue_name: str, task_id: str, channel: pika.adapters.blocking_connection.BlockingChannel):
+    print('Ожидание решения капчи в очереди')
+    while True:
+        method_frame, header_frame, body = channel.basic_get(queue_name)
+        if body:
+            json_body = json.loads(body.decode())
+            
+            if json_body.get('id')==task_id:
+                channel.basic_ack(method_frame.delivery_tag)
+                return json_body
+            else:
+                continue
+        else:
+            continue
+        # ставим небольшую задержку что бы не спамить сервер rabbitmq
+        time.sleep(0.5)
 
-    credentials = pika.PlainCredentials('visitor', 'password')
-    parameters = pika.ConnectionParameters(host,
-                                           5672,
-                                           'rucaptcha_vhost',
-                                           credentials)
-    connection = pika.BlockingConnection(parameters)
+def wait_answer_from_qeue(queue_name: str, task_id: str):
+    parameters = pika.URLParameters(f'amqp://{USERNAME}:password@{host}:5672/rucaptcha_vhost')
+    connection = pika.BlockingConnection(parameters=parameters)
+
     channel = connection.channel()
+    answer = wait_message(queue_name=queue_name, task_id=task_id, channel=channel)
 
-    channel.basic_consume(callback, queue=queue_name, arguments={'captcha_id': 6666})
-
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
-
+    print(answer.get('id'))
 
 async def register_new_qeue(route: str):
     async with aiohttp.ClientSession() as session:
@@ -46,7 +56,9 @@ if __name__ == '__main__':
     print('GOOOO!!!...')
     # создаём очередь на сервере с ключом
     loop.run_until_complete(register_new_qeue(route='register_key'))
-        
+    loop.close()    
     print('STOOOOP!!!...')
 
-    wait_queue_elements(RUCAPTCHA_KEY)
+    #wait_queue_elements(RUCAPTCHA_KEY)
+    # ожидание конкретного сообщения от сервера
+    wait_answer_from_qeue(RUCAPTCHA_KEY, TASK_ID)
