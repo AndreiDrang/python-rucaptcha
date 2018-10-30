43ed2dbe74031e36434aa7ba551450bd4af985b3
Andrei <drang.andray@gmail.com>
2018-10-30 20:26:04 +0300

    Добавление в конфиг параметров для подключения к callback серверу

--- python_rucaptcha/config.py
+++ python_rucaptcha/config.py
@@ -9,6 +9,19 @@ url_response_rucaptcha = "http://rucaptcha.com/res.php"
 # ключ приложения
 app_key = "1899"
 
+"""
+Параметры для callback
+"""
+# IP для работы callback`a
+HOST = '85.255.8.26'
+# PORT для работы callback`a
+PORT = 8001
+#данные для подключения к RabbitMQ на callback сервере
+RTMQ_USERNAME = 'hardworker_1'
+RTMQ_PASSWORD = 'password'
+RTMQ_HOST = '85.255.8.26'
+RTMQ_PORT = 5672
+RTMQ_VHOST = 'rucatpcha_vhost'
 
 """
 JSON возвращаемы пользователю после решения капчи
