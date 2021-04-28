d4e28a03fd94f71c972ed8ee4280d3aad7d5c9a3
Andrei D <drang.andray@gmail.com>
2021-04-28 04:19:24 +0300

    Update result_handler.py

--- python_rucaptcha/result_handler.py
+++ python_rucaptcha/result_handler.py
@@ -8,40 +8,49 @@ from .config import connect_generator
 from .serializer import ResponseSer, ServiceGetResponseSer
 
 
+def result_processing(captcha_response: ServiceGetResponseSer, result: ResponseSer) -> dict:
+    """
+    Function processing service response status values
+    """
+
+    # on error during solving
+    if captcha_response.status == 0:
+        result.error = True
+        result.errorBody = captcha_response.request
+
+    # if solving is success
+    elif captcha_response.status == 1:
+        result.captchaSolve = captcha_response.request
+
+        # if this is ReCaptcha v3 then we get it from the server
+        if captcha_response.user_check and captcha_response.user_score:
+            result = result.dict()
+            result.update(
+                {
+                    "user_check": captcha_response.user_check,
+                    "user_score": captcha_response.user_score,
+                }
+            )
+            return result
+
+    return result.dict()
+
+
 def get_sync_result(get_payload: dict, sleep_time: int, url_response: str, result: ResponseSer) -> dict:
-    # генератор для повторных попыток подключения к серверу
+    """
+    Function periodically send the SYNC request to service and wait for captcha solving result
+    """
+    # generator for repeated attempts to connect to the server
     connect_gen = connect_generator()
     while True:
         try:
-            # отправляем запрос на результат решения капчи
+            # send a request for the result of solving the captcha
             captcha_response = ServiceGetResponseSer(**requests.get(url_response, params=get_payload).json())
-            # если капча ещё не решена - ожидаем
+            # if the captcha has not been resolved yet, wait
             if captcha_response.request == "CAPCHA_NOT_READY":
                 time.sleep(sleep_time)
-
-            # при ошибке во время решения
-            elif captcha_response.status == 0:
-                result.error = True
-                result.errorBody = captcha_response.request
-                return result.dict()
-
-            # при решении капчи
-            elif captcha_response.status == 1:
-                result.captchaSolve = captcha_response.request
-
-                # если это ReCaptcha v3 то получаем от сервера
-                # дополнительные поля, с ID юзера и его счётом
-                if captcha_response.user_check and captcha_response.user_score:
-                    result = result.dict()
-                    result.update(
-                        {
-                            "user_check": captcha_response.user_check,
-                            "user_score": captcha_response.user_score,
-                        }
-                    )
-                    return result
-
-                return result.dict()
+            else:
+                return result_processing(captcha_response, result)
 
         except Exception as error:
             if next(connect_gen) < 4:
@@ -52,48 +61,31 @@ def get_sync_result(get_payload: dict, sleep_time: int, url_response: str, resul
                 return result.dict()
 
 
-async def get_async_result(get_payload: dict, sleep_time: int, url_response: str, result: dict):
-    # генератор для повторных попыток подключения к серверу
+async def get_async_result(get_payload: dict, sleep_time: int, url_response: str, result: ResponseSer) -> dict:
+    """
+    Function periodically send the ASYNC request to service and wait for captcha solving result
+    """
+    # generator for repeated attempts to connect to the server
     connect_gen = connect_generator()
-    # отправляем запрос на результат решения капчи, если не решена ожидаем
     async with aiohttp.ClientSession() as session:
         while True:
             try:
+                # send a request for the result of solving the captcha
                 async with session.get(url_response, params=get_payload) as resp:
                     captcha_response = await resp.json()
+                    captcha_response = ServiceGetResponseSer(**captcha_response)
 
-                    # если капча ещё не решена - ожидаем
-                    if captcha_response["request"] == "CAPCHA_NOT_READY":
+                    # if the captcha has not been resolved yet, wait
+                    if captcha_response.request == "CAPCHA_NOT_READY":
                         await asyncio.sleep(sleep_time)
 
-                    # при ошибке во время решения
-                    elif captcha_response["status"] == 0:
-                        result.update(
-                            {
-                                "error": True,
-                                "errorBody": captcha_response["request"],
-                            }
-                        )
-                        return result
-
-                    # при решении капчи
-                    elif captcha_response["status"] == 1:
-                        result.update({"captchaSolve": captcha_response["request"]})
-
-                        # если это ReCaptcha v3 то получаем от сервера
-                        # дополнительные поля, с ID юзера и его счётом
-                        if captcha_response.get("user_check") and captcha_response.get("user_score"):
-                            result.update(
-                                {
-                                    "user_check": captcha_response.get("user_check"),
-                                    "user_score": captcha_response.get("user_score"),
-                                }
-                            )
-                        return result
+                    else:
+                        return result_processing(captcha_response, result)
 
             except Exception as error:
                 if next(connect_gen) < 4:
                     await asyncio.sleep(2)
                 else:
-                    result.update({"error": True, "errorBody": {"text": error, "id": -1}})
-                    return result
+                    result.error = True
+                    result.errorBody = error
+                    return result.dict()
