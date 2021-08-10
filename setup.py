5bdca9c2ab410411c797c34e44abc91794207f41
Andrei <drang.andray@gmail.com>
2021-08-10 15:32:53 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ EMAIL = "drang.andray@gmail.com"
 AUTHOR = "AndreiDrang, redV0ID"
 REQUIRES_PYTHON = ">=3.6.0"
 VERSION = "3.0.1"
-REQUIRED = ["requests>=2.21.0", "aiohttp==3.*", "pika==1.*"]
+REQUIRED = ["requests>=2.21.0", "aiohttp>=3.7.4", "pika==1.*"]
 
 here = os.path.abspath(os.path.dirname(__file__))
 
