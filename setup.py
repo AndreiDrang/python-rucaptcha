95b5c5e38b7a00ee47e92253163cb7a7cc1e619b
Andrei D <drang.andray@gmail.com>
2021-10-11 00:00:27 +0300

    upd requirements

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ EMAIL = "drang.andray@gmail.com"
 AUTHOR = "AndreiDrang, redV0ID"
 REQUIRES_PYTHON = ">=3.6.0"
 VERSION = "4.0"
-REQUIRED = ["requests>=2.21.0", "aiohttp==3.*", "pika==1.*", "websockets==8.*", "pydantic==1.7.*", "tenacity==7.*"]
+REQUIRED = ["pika==1.2.*", "requests==2.*", "aiohttp==3.*", "websockets==9.*", "pydantic==1.8.*", "tenacity==7.*"]
 
 here = os.path.abspath(os.path.dirname(__file__))
 
