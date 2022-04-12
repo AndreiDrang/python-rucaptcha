38afacb2f311d4880960fbaeecad0935a4f983da
Andrei D <drang.andray@gmail.com>
2022-04-13 02:55:36 +0300

    Обновление метода Geetest

--- python_rucaptcha/GeeTest.py
+++ python_rucaptcha/GeeTest.py
@@ -2,234 +2,112 @@ import time
 import asyncio
 
 import aiohttp
-import requests
 
-from python_rucaptcha.config import app_key
-from python_rucaptcha.decorators import api_key_check, service_check
-from python_rucaptcha.result_handler import get_sync_result, get_async_result
+from .base import BaseCaptcha
+from .serializer import ServicePostResponseSer
+from .result_handler import get_sync_result, get_async_result
 
 
-class GeeTest:
-    def __init__(
-        self,
-        rucaptcha_key: str,
-        gt: str,
-        pageurl: str,
-        service_type: str = "2captcha",
-        api_server: str = None,
-        sleep_time: int = 15,
-        pingback: str = None,
-        **kwargs,
-    ):
-        """
-                Модуль отвечает за решение GeeTest
-                :param rucaptcha_key:  АПИ ключ капчи из кабинета пользователя
-        :param service_type: URL с которым будет работать программа, возможен вариант "2captcha"(стандартный)
-                                и "rucaptcha"
-        :param gt: Публичный ключ сайта (статический)
-        :param api_server: Домен API (обязателен для некоторых сайтов)
-        :param pageurl: Полный URL страницы с капчей GeeTest
-                :param sleep_time: Время ожидания решения
-        :param pingback: URL для решения капчи с ответом через callback
-        :param kwargs: Параметры для подключения к прокси.
-        """
-        # время ожидания решения капчи
-        self.sleep_time = sleep_time
-        # тип URL на с которым будет работать библиотека
-        self.service_type = service_type
+class GeeTest(BaseCaptcha):
+    """
+    Модуль отвечает за решение Geetest и Geetest v4
+    Подробнее о параметрах:
+        https://rucaptcha.com/api-rucaptcha#solving_geetest
+        https://rucaptcha.com/api-rucaptcha#geetest-v4
+    """
 
-        # пайлоад POST запроса на отправку капчи на сервер
-        self.post_payload = {
-            "key": rucaptcha_key,
-            "method": "geetest",
-            "json": 1,
-            "gt": gt,
-            "pageurl": pageurl,
-            "soft_id": app_key,
-        }
-        # добавляем api_server ксли передан
-        if api_server:
-            self.post_payload.update({"api_server": api_server})
-        # если был передан параметр для callback`a - добавляем его
-        if pingback:
-            self.post_payload.update({"pingback": pingback})
-        # Если переданы ещё параметры - вносим их в post_payload
-        if kwargs:
-            for key in kwargs:
-                self.post_payload.update({key: kwargs[key]})
-
-        # пайлоад GET запроса на получение результата решения капчи
-        self.get_payload = {"key": rucaptcha_key, "action": "get", "json": 1}
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        if exc_type:
-            return False
-        return True
-
-    @api_key_check
-    @service_check
-    def captcha_handler(self, challenge: str, **kwargs):
+    def captcha_handler(self, challenge: str = None, **kwargs):
         """
         Метод отвечает за передачу данных на сервер для решения капчи
-        :param challenge: Днамический ключ задания
-        :param kwargs: Для передачи дополнительных параметров
-
+        :param challenge: Значение параметра challenge найденное на сайте
+        :param kwargs: Параметры для библиотеки `requests`
         :return: Ответ на капчу в виде JSON строки с полями:
-            captchaSolve - решение капчи,
-            taskId - находится Id задачи на решение капчи, можно использовать при жалобах и прочем,
-            error - False - если всё хорошо, True - если есть ошибка,
-            errorBody - название ошибки
+                    captchaSolve - решение капчи,
+                    taskId - находится Id задачи на решение капчи,
+                    error - False - если всё хорошо, True - если есть ошибка,
+                    errorBody - название ошибки
         """
-        # result, url_request, url_response - задаются в декораторе `service_check`, после проверки переданного названия
-
-        # Если переданы ещё параметры - вносим их в get_payload
-        if kwargs:
-            for key in kwargs:
-                self.get_payload.update({key: kwargs[key]})
-
-        # добавляем в пайлоад параметры капчи переданные пользователем
         self.post_payload.update({"challenge": challenge})
-        # получаем ID капчи
-        captcha_id = requests.post(self.url_request, data=self.post_payload).json()
-
-        # если вернулся ответ с ошибкой то записываем её и возвращаем результат
-        if captcha_id["status"] == 0:
-            self.result.update({"error": True, "errorBody": captcha_id["request"]})
-            return self.result
-        # иначе берём ключ отправленной на решение капчи и ждём решения
-        else:
-            captcha_id = captcha_id["request"]
-            # вписываем в taskId ключ отправленной на решение капчи
-            self.result.update({"taskId": captcha_id})
-            # обновляем пайлоад, вносим в него ключ отправленной на решение капчи
-            self.get_payload.update({"id": captcha_id})
-
-            # если передан параметр `pingback` - не ждём решения капчи а возвращаем незаполненный ответ
-            if self.post_payload.get("pingback"):
-                return self.get_payload
-
+        try:
+            response = ServicePostResponseSer(
+                **self.session.post(self.params.url_request, data=self.post_payload, **kwargs).json()
+            )
+            if response.status == 1:
+                self.result.taskId = response.request
             else:
-                # Ожидаем решения капчи
-                time.sleep(self.sleep_time)
-                return get_sync_result(
-                    get_payload=self.get_payload,
-                    sleep_time=self.sleep_time,
-                    url_response=self.url_response,
-                    result=self.result,
-                )
+                self.result.error = True
+                self.result.errorBody = response.request
+        except Exception as error:
+            self.result.error = True
+            self.result.errorBody = error
+
+        # check for errors when downloading / transferring a file to the server
+        if self.result.error:
+            return self.result.dict(exclude_none=True)
+        # if all is ok - send captcha to service and wait solution
+        else:
+            # update payload - add captcha taskId
+            self.get_payload.update({"id": self.result.taskId})
+
+            # wait captcha solving
+            time.sleep(self.params.sleep_time)
+            return get_sync_result(
+                get_payload=self.get_payload,
+                sleep_time=self.params.sleep_time,
+                url_response=self.params.url_response,
+                result=self.result,
+            )
 
 
-class aioGeeTest:
-    def __init__(
-        self,
-        rucaptcha_key: str,
-        gt: str,
-        pageurl: str,
-        service_type: str = "2captcha",
-        api_server: str = None,
-        sleep_time: int = 15,
-        pingback: str = None,
-        **kwargs,
-    ):
-        """
-                Модуль отвечает за решение GeeTest
-                :param rucaptcha_key:  АПИ ключ капчи из кабинета пользователя
-        :param service_type: URL с которым будет работать программа, возможен вариант "2captcha"(стандартный)
-                                и "rucaptcha"
-        :param gt: Публичный ключ сайта (статический)
-        :param api_server: Домен API (обязателен для некоторых сайтов)
-        :param pageurl: Полный URL страницы с капчей GeeTest
-                :param sleep_time: Время ожидания решения
-        :param pingback: URL для решения капчи с ответом через callback
-                :param kwargs: Для передачи дополнительных параметров
-        """
-        # время ожидания решения капчи
-        self.sleep_time = sleep_time
-        # тип URL на с которым будет работать библиотека
-        self.service_type = service_type
+class aioGeeTest(BaseCaptcha):
+    """
+    Модуль отвечает за асинхронное решение Geetest и Geetest v4
+    Подробнее о параметрах:
+        https://rucaptcha.com/api-rucaptcha#solving_geetest
+        https://rucaptcha.com/api-rucaptcha#geetest-v4
+    """
 
-        # пайлоад POST запроса на отправку капчи на сервер
-        self.post_payload = {
-            "key": rucaptcha_key,
-            "method": "geetest",
-            "json": 1,
-            "gt": gt,
-            "pageurl": pageurl,
-            "soft_id": app_key,
-        }
-        # добавляем api_server ксли передан
-        if api_server:
-            self.post_payload.update({"api_server": api_server})
-        # если был передан параметр для callback`a - добавляем его
-        if pingback:
-            self.post_payload.update({"pingback": pingback})
-        # Если переданы ещё параметры - вносим их в post_payload
-        if kwargs:
-            for key in kwargs:
-                self.post_payload.update({key: kwargs[key]})
-
-        # пайлоад GET запроса на получение результата решения капчи
-        self.get_payload = {"key": rucaptcha_key, "action": "get", "json": 1}
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        if exc_type:
-            return False
-        return True
-
-    @api_key_check
-    @service_check
-    async def captcha_handler(self, challenge: str, **kwargs):
+    async def captcha_handler(self, challenge: str = None):
         """
         Метод отвечает за передачу данных на сервер для решения капчи
-        :param challenge: Днамический ключ задания
-        :param kwargs: Для передачи дополнительных параметров
-
+        :param challenge: Значение параметра challenge найденное на сайте
+        :param proxy: Прокси для aiohttp модуля
         :return: Ответ на капчу в виде JSON строки с полями:
-            captchaSolve - решение капчи,
-            taskId - находится Id задачи на решение капчи, можно использовать при жалобах и прочем,
-            error - False - если всё хорошо, True - если есть ошибка,
-            errorBody - название ошибки
+                    captchaSolve - решение капчи,
+                    taskId - находится Id задачи на решение капчи,
+                    error - False - если всё хорошо, True - если есть ошибка,
+                    errorBody - название ошибки
         """
-        # result, url_request, url_response - задаются в декораторе `service_check`, после проверки переданного названия
+        self.post_payload.update({"challenge": challenge})
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.post(self.params.url_request, data=self.post_payload) as resp:
+                    response_json = await resp.json()
+                    response = ServicePostResponseSer(**response_json)
 
-        # Если переданы ещё параметры - вносим их в get_payload
-        if kwargs:
-            for key in kwargs:
-                self.get_payload.update({key: kwargs[key]})
-
-        # получаем ID капчи
-        async with aiohttp.ClientSession() as session:
-            async with session.post(self.url_request, data=self.post_payload) as resp:
-                captcha_id = await resp.json()
-
-        # если вернулся ответ с ошибкой то записываем её и возвращаем результат
-        if captcha_id["status"] == 0:
-            self.result.update({"error": True, "errorBody": captcha_id["request"]})
-            return self.result
-        # иначе берём ключ отправленной на решение капчи и ждём решения
-        else:
-            captcha_id = captcha_id["request"]
-            # вписываем в taskId ключ отправленной на решение капчи
-            self.result.update({"taskId": captcha_id})
-            # обновляем пайлоад, вносим в него ключ отправленной на решение капчи
-            self.get_payload.update({"id": captcha_id})
-
-            # если передан параметр `pingback` - не ждём решения капчи а возвращаем незаполненный ответ
-            if self.post_payload.get("pingback"):
-                return self.get_payload
-
+            if response.status == 1:
+                self.result.taskId = response.request
             else:
-                # Ожидаем решения капчи
-                await asyncio.sleep(self.sleep_time)
-                return await get_async_result(
-                    get_payload=self.get_payload,
-                    sleep_time=self.sleep_time,
-                    url_response=self.url_response,
-                    result=self.result,
-                )
+                self.result.error = True
+                self.result.errorBody = response.request
+
+        except Exception as error:
+            self.result.error = True
+            self.result.errorBody = error
+
+        # check for errors when downloading / transferring a file to the server
+        if self.result.error:
+            return self.result.dict(exclude_none=True)
+        # if all is ok - send captcha to service and wait solution
+        else:
+            # update payload - add captcha taskId
+            self.get_payload.update({"id": self.result.taskId})
+
+            # wait captcha solving
+            await asyncio.sleep(self.params.sleep_time)
+            return await get_async_result(
+                get_payload=self.get_payload,
+                sleep_time=self.params.sleep_time,
+                url_response=self.params.url_response,
+                result=self.result,
+            )
--- python_rucaptcha/base.py
+++ python_rucaptcha/base.py
@@ -1,4 +1,41 @@
+import requests
+from requests.adapters import HTTPAdapter
+
+from . import enums
+from .serializer import ResponseSer, GetRequestSer, PostRequestSer, CaptchaOptionsSer
+
+
 class BaseCaptcha:
+    def __init__(
+        self, rucaptcha_key: str, sleep_time: int = 15, service_type: str = enums.ServicesEnm.TWOCAPTCHA.value, **kwargs
+    ):
+        """
+        :param rucaptcha_key:  АПИ ключ капчи из кабинета пользователя
+        :param sleep_time: Вермя ожидания решения капчи
+        :param service_type: URL с которым будет работать программа, возможен вариант "2captcha"(стандартный)
+                             и "rucaptcha"
+        :param kwargs: Служит для передачи необязательных параметров в пайлоад для запроса к RuCaptcha
+        """
+        # assign args to validator
+        self.params = CaptchaOptionsSer(**locals())
+
+        # prepare POST payload
+        self.post_payload = PostRequestSer(key=self.params.rucaptcha_key, method="geetest").dict(by_alias=True)
+        # prepare GET payload
+        self.get_payload = GetRequestSer(key=self.params.rucaptcha_key).dict(by_alias=True, exclude_none=True)
+        # prepare result payload
+        self.result = ResponseSer()
+
+        # If more parameters are passed, add them to post_payload
+        if kwargs:
+            for key in kwargs:
+                self.post_payload.update({key: kwargs[key]})
+
+        # prepare session
+        self.session = requests.Session()
+        self.session.mount("http://", HTTPAdapter(max_retries=5))
+        self.session.mount("https://", HTTPAdapter(max_retries=5))
+
     def __enter__(self):
         return self
 
