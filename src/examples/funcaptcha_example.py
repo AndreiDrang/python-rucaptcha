71ca3841aa254add48e76a83ad0c4ce19ec693c3
Andrei <drang.andray@gmail.com>
2022-07-03 18:14:50 +0300

    Update funcaptcha_example.py

--- src/examples/funcaptcha_example.py
+++ src/examples/funcaptcha_example.py
@@ -1,17 +1,34 @@
+import asyncio
+
 from python_rucaptcha.enums import FunCaptchaEnm
-from python_rucaptcha.FunCaptcha import FunCaptcha
+from python_rucaptcha.FunCaptcha import FunCaptcha, aioFunCaptcha
 
 # Rucaptcha API Key from your account
-RUCAPTCHA_KEY = "ad91111111111111768608fa758570"
+RUCAPTCHA_KEY = "ad9053f111111111111111fa758570"
 
 
 publickey = "69A21A01-CC7B-B9C6-1111-E7FA06677FFC"
 pageurl = "https://api.funcaptcha.com/fc/api/nojs/"
 surl = "https://client-api.arkoselabs.com"
 
-
-with FunCaptcha(
+fun_captcha = FunCaptcha(
     rucaptcha_key=RUCAPTCHA_KEY, pageurl=pageurl, publickey=publickey, surl=surl, method=FunCaptchaEnm.FUNCAPTCHA.value
-) as fun_captcha:
-    result = fun_captcha.captcha_handler()
-    print(result)
+)
+result = fun_captcha.captcha_handler()
+print(result)
+
+
+async def run():
+    try:
+        result = await aioFunCaptcha(
+            rucaptcha_key=RUCAPTCHA_KEY,
+            pageurl=pageurl,
+            publickey=publickey,
+            surl=surl,
+            method=FunCaptchaEnm.FUNCAPTCHA.value).captcha_handler()
+        print(result)
+    except Exception as err:
+        print(err)
+
+
+asyncio.run(run())
