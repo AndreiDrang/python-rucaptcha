41f89a332bf156a77868ae441d22be99b1956eaa
Andrei <drang.andray@gmail.com>
2022-08-05 17:59:56 +0300

    Update key_captcha_example.py

--- src/examples/key_captcha_example.py
+++ src/examples/key_captcha_example.py
@@ -1,184 +1,45 @@
 import asyncio
 
-import requests
-import CallbackClient
+from src.python_rucaptcha.enums import KeyCaptchaEnm
+from src.python_rucaptcha.KeyCaptcha import KeyCaptcha, aioKeyCaptcha
 
-from python_rucaptcha import KeyCaptcha, RuCaptchaControl
+# Rucaptcha API Key from your account
+RUCAPTCHA_KEY = "ad9053f111111111111111fa758570"
 
-"""
-Этот пример показывает то как нужно работать с модулем для распознования KeyCaptcha - капчи пазла,
-на примере нашего сайта.
-В общем случае вам потребуется получение:
-1. Получить данные капчи-пазла с сайта с этой капчёй.
-Пример страницы для тестов - https://www.keycaptcha.com/signup/
-Данные которые извлекаются с данной страницы и используются для дальнейшей работы:
-s_s_c_user_id=15,
-s_s_c_session_id='8f460599bebe02cb0dd096b1fe70b089',
-s_s_c_web_server_sign ='edd2c221c05aece19f6db93a36b42272',
-s_s_c_web_server_sign2 ='15989edaad1b4dc056ec8fa05abc7c9a',
-page_url ='https://www.keycaptcha.com/signup/'
+s_s_c_user_id = "184015"
+s_s_c_session_id = "0917788cad24ad3a69813c4fcd556061"
+s_s_c_web_server_sign = "02f7f9669f1269595c4c69bcd4a3c52e"
+s_s_c_web_server_sign2 = "d888700f6f324ec0f32b44c32c50bde1"
+pageurl = "https://rucaptcha.com/demo/keycaptcha"
 
-2. Передать извлечённые параметры в метод KeyCaptcha - captcha_handler(....)
-"""
-"""
-В общем случаи запрос на решение капчи-пазла выглядит следующим способом
-!!!Все параметры являются обязательными!!!
-"""
-RUCAPTCHA_KEY = "2597d7cb1f9435a3b531ac283ce987d5"
-
-
-"""
-contextmanager пример
-"""
-
-# синхронный пример contextmanager
-with KeyCaptcha.KeyCaptcha(rucaptcha_key=RUCAPTCHA_KEY) as key_captcha:
-    result = key_captcha.captcha_handler(
-        key_params={
-            "s_s_c_user_id": 15,
-            "s_s_c_session_id": "8f460599bebe02cb0dd096b1fe70b089",
-            "s_s_c_web_server_sign": "edd2c221c05aece19f6db93a36b42272",
-            "s_s_c_web_server_sign2": "15989edaad1b4dc056ec8fa05abc7c9a",
-            "pageurl": "https://www.keycaptcha.com/signup/",
-        }
-    )
-    print(result)
-
-# асинхронный пример contextmanager
-async def aiocontext():
-    with KeyCaptcha.aioKeyCaptcha(rucaptcha_key=RUCAPTCHA_KEY) as key_captcha:
-        result = await key_captcha.captcha_handler(
-            key_params={
-                "s_s_c_user_id": 15,
-                "s_s_c_session_id": "8f460599bebe02cb0dd096b1fe70b089",
-                "s_s_c_web_server_sign": "edd2c221c05aece19f6db93a36b42272",
-                "s_s_c_web_server_sign2": "15989edaad1b4dc056ec8fa05abc7c9a",
-                "pageurl": "https://www.keycaptcha.com/signup/",
-            }
-        )
-        print(result)
-
-
-if __name__ == "__main__":
-    loop = asyncio.new_event_loop()
-    loop.run_until_complete(aiocontext())
-    loop.close()
-
-answer = KeyCaptcha.KeyCaptcha(rucaptcha_key=RUCAPTCHA_KEY).captcha_handler(
-    key_params={
-        "s_s_c_user_id": 15,
-        "s_s_c_session_id": "8f460599bebe02cb0dd096b1fe70b089",
-        "s_s_c_web_server_sign": "edd2c221c05aece19f6db93a36b42272",
-        "s_s_c_web_server_sign2": "15989edaad1b4dc056ec8fa05abc7c9a",
-        "pageurl": "https://www.keycaptcha.com/signup/",
-    }
+lemin_captcha = KeyCaptcha(
+    rucaptcha_key=RUCAPTCHA_KEY,
+    pageurl=pageurl,
+    s_s_c_user_id=s_s_c_user_id,
+    s_s_c_session_id=s_s_c_session_id,
+    s_s_c_web_server_sign=s_s_c_web_server_sign,
+    s_s_c_web_server_sign2=s_s_c_web_server_sign2,
+    method=KeyCaptchaEnm.KEYCAPTCHA.value,
 )
+result = lemin_captcha.captcha_handler()
 
-"""
-answer - это JSON строка с соответствующими полями
-
-user_answer_... - это JSON строка с соответствующими полями
-captchaSolve - решение капчи,
-taskId - находится Id задачи на решение капчи, можно использовать при жалобах и прочем,
-error - False - если всё хорошо, True - если есть ошибка,
-errorBody - полная информация об ошибке: 
-    {
-        text - Развернётое пояснение ошибки
-        id - уникальный номер ошибка в ЭТОЙ бибилотеке
-    }
-"""
-
-# капча решена верно, ошибка = 0
-if answer["error"] == 0:
-    # решение капчи
-    print(answer["captchaSolve"])
-    print(answer["taskId"])
-# во время решения капчи возникли ошибки, ошибка = 1
-elif answer["error"] == 1:
-    # Тело ошибки, если есть
-    print(answer["errorBody"])
-
-"""
-Пример асинхронного кода
-"""
+print(result)
 
 
 async def run():
     try:
-        answer = await KeyCaptcha.aioKeyCaptcha(rucaptcha_key=RUCAPTCHA_KEY).captcha_handler(
-            key_params={
-                "s_s_c_user_id": 15,
-                "s_s_c_session_id": "8f460599bebe02cb0dd096b1fe70b089",
-                "s_s_c_web_server_sign": "edd2c221c05aece19f6db93a36b42272",
-                "s_s_c_web_server_sign2": "15989edaad1b4dc056ec8fa05abc7c9a",
-                "pageurl": "https://www.keycaptcha.com/signup/",
-            }
-        )
-
-        if not answer["error"]:
-            # решение капчи
-            print(answer["captchaSolve"])
-            print(answer["taskId"])
-        elif answer["error"]:
-            # Тело ошибки, если есть
-            print(answer["errorBody"])
+        lemin_captcha = await aioKeyCaptcha(
+            rucaptcha_key=RUCAPTCHA_KEY,
+            pageurl=pageurl,
+            s_s_c_user_id=s_s_c_user_id,
+            s_s_c_session_id=s_s_c_session_id,
+            s_s_c_web_server_sign=s_s_c_web_server_sign,
+            s_s_c_web_server_sign2=s_s_c_web_server_sign2,
+            method=KeyCaptchaEnm.KEYCAPTCHA.value,
+        ).captcha_handler()
+        print(lemin_captcha)
     except Exception as err:
-        print(f"ERRRRORORO - {err}")
+        print(err)
 
 
-if __name__ == "__main__":
-    loop = asyncio.new_event_loop()
-    loop.run_until_complete(run())
-    loop.close()
-
-
-"""
-Callback пример
-"""
-# нужно передать IP/URL ранее зарегистрированного сервера
-server_ip = "pythoncaptcha.tech"
-# и по желанию - порт на сервере который слушает ваше веб-приложение
-server_port = 8001
-# регистрация нового домена для callback/pingback
-answer = RuCaptchaControl.RuCaptchaControl(rucaptcha_key=RUCAPTCHA_KEY).additional_methods(
-    action="add_pingback", addr=f"http://{server_ip}:{server_port}/", json=1
-)
-print(answer)
-
-# нужно придумать ЛЮБОЕ сложное название очереди(15+ знаков подойдёт)
-queue_name = "ba86e77f9007_andrei_drang_7436e7_key_captcha_442674_cute_queue"
-# регистрируем очередь на callback сервере
-answer = requests.post(
-    f"http://{server_ip}:{server_port}/register_key",
-    json={"key": queue_name, "vhost": "rucaptcha_vhost"},
-)
-
-# если очередь зарегистрирована
-if answer.text == "OK":
-    # IP адрес должен быть ЗАРАНЕЕ зарегистрирован в системе (подробонсти смотри в `CaptchaTester/rucaptcha_control_example.py`)
-    # создаём задание на сервере, ответ на которое придёт на заданный pingback URL в виде POST запроса
-    task_creation_answer = KeyCaptcha.KeyCaptcha(
-        rucaptcha_key=RUCAPTCHA_KEY,
-        pingback=f"pythoncaptcha.tech:8001/rucaptcha/key_captcha/{queue_name}",
-    ).captcha_handler(
-        key_params={
-            "s_s_c_user_id": 15,
-            "s_s_c_session_id": "8f460599bebe02cb0dd096b1fe70b089",
-            "s_s_c_web_server_sign": "edd2c221c05aece19f6db93a36b42272",
-            "s_s_c_web_server_sign2": "15989edaad1b4dc056ec8fa05abc7c9a",
-            "pageurl": "https://www.keycaptcha.com/signup/",
-        }
-    )
-    print(task_creation_answer)
-
-    # подключаемся к серверу и ждём решения капчи из кеша
-    callback_server_response = CallbackClient.CallbackClient(task_id=task_creation_answer.get("id")).captcha_handler()
-
-    print(callback_server_response)
-
-    # подключаемся к серверу и ждём решения капчи из RabbitMQ queue
-    callback_server_response = CallbackClient.CallbackClient(
-        task_id=task_creation_answer.get("id"), queue_name=queue_name, call_type="queue"
-    ).captcha_handler()
-
-    print(callback_server_response)
+asyncio.run(run())
