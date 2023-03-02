67cfc69e541224ad15d9ff4b42da9ca195134965
Andrei <drang.andray@gmail.com>
2023-03-02 13:49:31 +0300

    Update amazon_waf.py

--- src/python_rucaptcha/amazon_waf.py
+++ src/python_rucaptcha/amazon_waf.py
@@ -2,37 +2,7 @@ from python_rucaptcha.core.base import BaseCaptcha
 from python_rucaptcha.core.enums import AmazonWAFCaptchaEnm
 
 
-class BaseAmazonWAF(BaseCaptcha):
-    """
-    The class is used to work with Amazon WAF
-
-    Args:
-        rucaptcha_key: User API key
-        pageurl: Full URL of the captcha page
-        sitekey: Key value from the page
-        iv: Value iv from the page
-        context: Value of context from page
-
-    Examples:
-        >>> AmazonWAF(rucaptcha_key="aa9011f31111181111168611f1151122",
-        ...           pageurl="https://page-with-waf.com/",
-        ...           sitekey="some-site-key",
-        ...           iv="some-iv-value",
-        ...           context="some-context-value").captcha_handler()
-        {
-            'serverAnswer': {},
-            'captchaSolve': 'eyJ0e......jNuSFqtyP4Ho',
-            'taskId': '7111111984',
-            'error': False,
-            'errorBody': None
-        }
-
-    Returns:
-        Dict with full server response
-
-    Notes:
-        https://rucaptcha.com/api-rucaptcha#amazon-waf
-    """
+class AmazonWAF(BaseCaptcha):
 
     def __init__(
         self,
@@ -45,6 +15,37 @@ class BaseAmazonWAF(BaseCaptcha):
         *args,
         **kwargs,
     ):
+        """
+        The class is used to work with Amazon WAF
+
+        Args:
+            rucaptcha_key: User API key
+            pageurl: Full URL of the captcha page
+            sitekey: Key value from the page
+            iv: Value iv from the page
+            context: Value of context from page
+            method: Captcha type
+
+        Examples:
+            >>> AmazonWAF(rucaptcha_key="aa9011f31111181111168611f1151122",
+            ...           pageurl="https://page-with-waf.com/",
+            ...           sitekey="some-site-key",
+            ...           iv="some-iv-value",
+            ...           context="some-context-value").captcha_handler()
+            {
+                'serverAnswer': {},
+                'captchaSolve': 'eyJ0e......jNuSFqtyP4Ho',
+                'taskId': '7111111984',
+                'error': False,
+                'errorBody': None
+            }
+
+        Returns:
+            Dict with full server response
+
+        Notes:
+            https://rucaptcha.com/api-rucaptcha#amazon-waf
+        """
         super().__init__(rucaptcha_key=rucaptcha_key, method=method, *args, **kwargs)
 
         self.post_payload.update({"sitekey": sitekey, "pageurl": pageurl, "iv": iv, "context": context})
@@ -54,8 +55,6 @@ class BaseAmazonWAF(BaseCaptcha):
             raise ValueError(f"Invalid method parameter set, available - {AmazonWAFCaptchaEnm.list_values()}")
 
 
-class AmazonWAF(BaseAmazonWAF):
-    __doc__ = BaseAmazonWAF.__doc__
 
     def captcha_handler(self, **kwargs) -> dict:
         """
