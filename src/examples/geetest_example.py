f7185ec393084975281661870bd93fa236d7b476
Andrei <drang.andray@gmail.com>
2023-03-10 22:43:01 +0300

    Update geetest_example.py

--- src/examples/geetest_example.py
+++ src/examples/geetest_example.py
@@ -1,7 +1,7 @@
 import asyncio
 
-from enums import GeetestEnm
-from GeeTest import GeeTest, aioGeeTest
+from python_rucaptcha.core.enums import GeetestEnm
+from python_rucaptcha.gee_test import GeeTest
 
 # Rucaptcha API Key from your account
 RUCAPTCHA_KEY = "ad9053f31111a8171111111111a758570"
@@ -22,14 +22,14 @@ print(gt.captcha_handler(challenge="12345678abc90123d45678ef90123a456b"))
 
 
 async def run():
-    gt = aioGeeTest(
+    gt = GeeTest(
         rucaptcha_key=RUCAPTCHA_KEY,
         gt="f1ab2cdefa3456789012345b6c78d90e",
         method=GeetestEnm.GEETEST.value,
         pageurl="https://www.site.com/page/",
         api_server="api-na.geetest.com",
     )
-    result = await gt.captcha_handler(challenge="12345678abc90123d45678ef90123a456b")
+    result = await gt.aio_captcha_handler(challenge="12345678abc90123d45678ef90123a456b")
     print(result)
 
 
@@ -50,13 +50,13 @@ print(gt.captcha_handler())
 
 
 async def run():
-    gt = aioGeeTest(
+    gt = GeeTest(
         rucaptcha_key=RUCAPTCHA_KEY,
         method=GeetestEnm.GEETEST_V4.value,
         pageurl="https://rucaptcha.com/demo/geetest-v4",
         captcha_id="e392e1d7fd421dc63325744d5a2b9c73",
     )
-    result = await gt.captcha_handler()
+    result = await gt.aio_captcha_handler()
     print(result)
 
 
