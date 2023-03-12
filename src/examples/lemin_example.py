5d60ef88d5911a7e258f694c93f40bb99ddc40f9
Andrei <drang.andray@gmail.com>
2023-03-13 00:11:44 +0300

    Update lemin_example.py

--- src/examples/lemin_example.py
+++ src/examples/lemin_example.py
@@ -1,15 +1,14 @@
 import asyncio
 
 from src.python_rucaptcha.core.enums import LeminCroppedCaptchaEnm
-from src.python_rucaptcha.LeminCroppedCaptcha import LeminCroppedCaptcha, aioLeminCroppedCaptcha
+from src.python_rucaptcha.lemin_cropped_captcha import LeminCroppedCaptcha
 
 # Rucaptcha API Key from your account
 RUCAPTCHA_KEY = "ad9053f111111111111111fa758570"
 
-
 pageurl = "https://dashboard.leminnow.com/auth/signup"
-api_server = "https://api.leminnow.com/"
-div_id = "jOfjiAwLlemin-captcha-input-box"
+api_server = "api.leminnow.com"
+div_id = "lemin-cropped-captcha"
 captcha_id = "CROPPED_099216d_8ba061383fa24ef498115023aa7189d4"
 
 lemin_captcha = LeminCroppedCaptcha(
@@ -27,14 +26,14 @@ print(result)
 
 async def run():
     try:
-        lemin_captcha = await aioLeminCroppedCaptcha(
+        lemin_captcha = await LeminCroppedCaptcha(
             rucaptcha_key=RUCAPTCHA_KEY,
             pageurl=pageurl,
             captcha_id=captcha_id,
             div_id=div_id,
             method=LeminCroppedCaptchaEnm.LEMIN.value,
             api_server=api_server,
-        ).captcha_handler()
+        ).aio_captcha_handler()
         print(lemin_captcha)
     except Exception as err:
         print(err)
