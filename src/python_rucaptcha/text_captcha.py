5ed6a7a35ff4dcdbda9462d8e3bfc2919f573dce
Andrei <drang.andray@gmail.com>
2023-03-13 00:17:16 +0300

    Update text_captcha.py

--- src/python_rucaptcha/text_captcha.py
+++ src/python_rucaptcha/text_captcha.py
@@ -2,44 +2,6 @@ from python_rucaptcha.core.base import BaseCaptcha
 
 
 class TextCaptcha(BaseCaptcha):
-    """
-    The class is used to work with TextCaptcha.
-    Solve description:
-        https://rucaptcha.com/api-rucaptcha#solving_text_captcha
-
-    Args:
-        rucaptcha_key: User API key
-        language: Captcha text lang:
-                    0 - not defined
-                    1 - captcha contains only Cyrillic
-                    2 - captcha contains only latin characters
-
-    Examples:
-        >>> TextCaptcha(rucaptcha_key="aa9011f31111181111168611f1151122",
-        ...             language=2).captcha_handler(textcaptcha="Our planet name?")
-        {
-            'captchaSolve': 'earth',
-            'taskId': '73043008354',
-            'error': False,
-            'errorBody': None
-        }
-
-        >>> TextCaptcha(rucaptcha_key="aa9011f31111181111168611f1151122")\
-        ...             .captcha_handler(textcaptcha="Our planet name?")
-        {
-            'captchaSolve': 'earth',
-            'taskId': '73043008354',
-            'error': False,
-            'errorBody': None
-        }
-
-    Returns:
-        Dict with full server response
-
-    Notes:
-        https://rucaptcha.com/api-rucaptcha#solving_text_captcha
-    """
-
     def __init__(
         self,
         rucaptcha_key: str,
@@ -47,6 +9,44 @@ class TextCaptcha(BaseCaptcha):
         *args,
         **kwargs,
     ):
+        """
+        The class is used to work with TextCaptcha.
+        Solve description:
+            https://rucaptcha.com/api-rucaptcha#solving_text_captcha
+
+        Args:
+            rucaptcha_key: User API key
+            language: Captcha text lang:
+                        0 - not defined
+                        1 - captcha contains only Cyrillic
+                        2 - captcha contains only latin characters
+
+        Examples:
+            >>> TextCaptcha(rucaptcha_key="aa9011f31111181111168611f1151122",
+            ...             language=2).captcha_handler(textcaptcha="Our planet name?")
+            {
+                'captchaSolve': 'earth',
+                'taskId': '73043008354',
+                'error': False,
+                'errorBody': None
+            }
+
+            >>> TextCaptcha(rucaptcha_key="aa9011f31111181111168611f1151122"
+            ...             ).captcha_handler(textcaptcha="Our planet name?")
+            {
+                'captchaSolve': 'earth',
+                'taskId': '73043008354',
+                'error': False,
+                'errorBody': None
+            }
+
+        Returns:
+            Dict with full server response
+
+        Notes:
+            https://rucaptcha.com/api-rucaptcha#solving_text_captcha
+        """
+
         super().__init__(rucaptcha_key=rucaptcha_key, *args, **kwargs)
 
         self.post_payload.update({"language": language})
@@ -69,11 +69,7 @@ class TextCaptcha(BaseCaptcha):
             }
 
         Returns:
-            Response to captcha as JSON string with fields:
-                 captchaSolve - captcha solution,
-                 taskId - finds the ID of the task to solve the captcha,
-                 error - False - if everything is fine, True - if there is an error,
-                 errorBody - error name
+            Dict with full server response
 
         Notes:
             Check class docstirng for more info
@@ -87,7 +83,7 @@ class TextCaptcha(BaseCaptcha):
         Asynchronous method for captcha solving
 
         Args:
-            :param textcaptcha: Captcha text
+            textcaptcha: Captcha text
 
         Examples:
             >>> await TextCaptcha(rucaptcha_key="aa9011f31111181111168611f1151122",
@@ -100,11 +96,7 @@ class TextCaptcha(BaseCaptcha):
             }
 
         Returns:
-            Response to captcha as JSON string with fields:
-                 captchaSolve - captcha solution,
-                 taskId - finds the ID of the task to solve the captcha,
-                 error - False - if everything is fine, True - if there is an error,
-                 errorBody - error name
+            Dict with full server response
 
         Notes:
             Check class docstirng for more info
