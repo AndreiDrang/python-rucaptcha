f736fe0ebd22499221c5dfe0938a707b418cf25c
Andrei <drang.andray@gmail.com>
2023-03-13 02:22:49 +0300

    Update image_captcha_example.py

--- src/examples/image_captcha_example.py
+++ src/examples/image_captcha_example.py
@@ -1,12 +1,12 @@
 import asyncio
 
-from src.python_rucaptcha.ImageCaptcha import ImageCaptcha, aioImageCaptcha
+from src.python_rucaptcha.image_captcha import ImageCaptcha
 
 # Rucaptcha API Key from your account
 RUCAPTCHA_KEY = "ad911111111111ca81755768608fa758570"
 
 captcha_file = "src/examples/088636.png"
-captcha_url = "https://pythoncaptcha.xyz/static/image/common_image_example/862963.png"
+captcha_url = "https://rucaptcha.com/dist/web/99581b9d446a509a0a01954438a5e36a.jpg"
 
 
 image_captcha = ImageCaptcha(rucaptcha_key=RUCAPTCHA_KEY)
@@ -21,12 +21,12 @@ print(result)
 
 
 async def run():
-    image_captcha = aioImageCaptcha(rucaptcha_key=RUCAPTCHA_KEY)
-    result = await image_captcha.captcha_handler(captcha_file=captcha_file)
+    image_captcha = ImageCaptcha(rucaptcha_key=RUCAPTCHA_KEY)
+    result = await image_captcha.aio_captcha_handler(captcha_file=captcha_file)
     print(result)
 
-    image_captcha = aioImageCaptcha(rucaptcha_key=RUCAPTCHA_KEY)
-    result = await image_captcha.captcha_handler(captcha_link=captcha_url)
+    image_captcha = ImageCaptcha(rucaptcha_key=RUCAPTCHA_KEY)
+    result = await image_captcha.aio_captcha_handler(captcha_link=captcha_url)
     print(result)
 
 
