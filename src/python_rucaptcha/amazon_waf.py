ef7c9d6b1e31b6b71c5125104a79a21de4ffc8d7
Andrei <drang.andray@gmail.com>
2023-12-07 00:19:43 +0300

    Update amazon_waf.py

--- src/python_rucaptcha/amazon_waf.py
+++ src/python_rucaptcha/amazon_waf.py
@@ -26,8 +26,8 @@ class AmazonWAF(BaseCaptcha):
 
         Examples:
             >>> AmazonWAF(rucaptcha_key="aa9011f31111181111168611f1151122",
-            ...           pageurl="https://page-with-waf.com/",
-            ...           sitekey="some-site-key",
+            ...           websiteURL="https://page-with-waf.com/",
+            ...           websiteKey="some-site-key",
             ...           iv="some-iv-value",
             ...           context="some-context-value").captcha_handler()
             {
@@ -38,8 +38,8 @@ class AmazonWAF(BaseCaptcha):
             }
 
             >>> AmazonWAF(rucaptcha_key="aa9011f31111181111168611f1151122",
-            ...           pageurl="https://page-with-waf.com/",
-            ...           sitekey="some-site-key",
+            ...           websiteURL="https://page-with-waf.com/",
+            ...           websiteKey="some-site-key",
             ...           iv="some-iv-value",
             ...           context="some-context-value").aio_captcha_handler()
             {
@@ -60,30 +60,22 @@ class AmazonWAF(BaseCaptcha):
         # check user params
         if method not in AmazonWAFCaptchaEnm.list_values():
             raise ValueError(f"Invalid method parameter set, available - {AmazonWAFCaptchaEnm.list_values()}")
+        # insert `gt` param to payload
+        self.create_task_payload["task"].update(
+            {
+                "websiteURL": websiteURL,
+                "websiteKey": websiteKey,
+                "iv": iv,
+                "context": context,
+            }
+        )
 
     def captcha_handler(self, **kwargs) -> dict:
         """
         Synchronous method for captcha solving
 
-        Examples:
-            >>> AmazonWAF(rucaptcha_key="aa9011f31111181111168611f1151122",
-            ...           pageurl="https://page-with-waf.com/",
-            ...           sitekey="some-site-key",
-            ...           iv="some-iv-value",
-            ...           context="some-context-value").captcha_handler()
-            {
-                'captchaSolve': 'eyJ0e......jNuSFqtyP4Ho',
-                'taskId': '7111111984',
-                'error': False,
-                'errorBody': None
-            }
-
         Returns:
-            Response to captcha as JSON string with fields:
-                 captchaSolve - captcha solution,
-                 taskId - finds the ID of the task to solve the captcha,
-                 error - False - if everything is fine, True - if there is an error,
-                 errorBody - error name
+            Dict with full server response
 
         Notes:
             Check class docstirng for more info
@@ -95,25 +87,8 @@ class AmazonWAF(BaseCaptcha):
         """
         Asynchronous method for captcha solving
 
-        Examples:
-            >>> await AmazonWAF(rucaptcha_key="aa9011f31111181111168611f1151122",
-            ...           pageurl="https://page-with-waf.com/",
-            ...           sitekey="some-site-key",
-            ...           iv="some-iv-value",
-            ...           context="some-context-value").aio_captcha_handler()
-            {
-                'captchaSolve': 'eyJ0e......jNuSFqtyP4Ho',
-                'taskId': '7111111984',
-                'error': False,
-                'errorBody': None
-            }
-
         Returns:
-            Response to captcha as JSON string with fields:
-                 captchaSolve - captcha solution,
-                 taskId - finds the ID of the task to solve the captcha,
-                 error - False - if everything is fine, True - if there is an error,
-                 errorBody - error name
+            Dict with full server response
 
         Notes:
             Check class docstirng for more info
