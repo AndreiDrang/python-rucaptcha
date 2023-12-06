02e2c4e5aaa1cef923dfbf6937fe1e4fb9f20501
Andrei <drang.andray@gmail.com>
2023-12-07 00:32:25 +0300

    Update capy_puzzle.py

--- src/python_rucaptcha/capy_puzzle.py
+++ src/python_rucaptcha/capy_puzzle.py
@@ -4,81 +4,99 @@ from .core.enums import CapyPuzzleEnm
 
 class CapyPuzzle(BaseCaptcha):
     def __init__(
-        self, pageurl: str, captchakey: str, method: str = CapyPuzzleEnm.CapyTaskProxyless.value, *args, **kwargs
+        self, websiteURL: str, websiteKey: str, method: str = CapyPuzzleEnm.CapyTaskProxyless.value, *args, **kwargs
     ):
         """
         The class is used to work with CapyPuzzle.
 
         Args:
             rucaptcha_key: User API key
-            pageurl: Full URL of the captcha page
-            captchakey: The value of the `captchakey` parameter you found in the code of the page
+            websiteURL: Full URL of the captcha page
+            websiteKey: The value of the `captchakey` parameter you found in the code of the page
             method: Captcha type
 
         Examples:
             >>> CapyPuzzle(rucaptcha_key="aa9011f31111181111168611f1151122",
-            ...             captchakey="PUZZLE_Cme4hZLjuZRMYC3uh14C52D3uNms5w",
-            ...             pageurl="https://www.capy.me/account/register/",
+            ...             websiteKey="PUZZLE_Cme4hZLjuZRMYC3uh14C52D3uNms5w",
+            ...             websiteURL="https://www.capy.me/account/register/",
             ...             method=CapyPuzzleEnm.CapyTaskProxyless.value,
             ...             api_server="https://jp.api.capy.me/",
             ...             version="puzzle",
             ...             ).captcha_handler()
             {
-               "captchaSolve": {
-                  "captchakey": "PUZZLE_C...w",
-                  "challengekey": "Uf....It",
-                  "answer": "26x...x9mx"
+               "errorId":0,
+               "status":"ready",
+               "solution":{
+                  "captchakey":"PUZZLE_Abc1dEFghIJKLM2no34P56q7rStu8v",
+                  "challengekey":"qHAPtn68KTnXFM8VQ3mtYRtmy3cSKuHJ",
+                  "answer":"0xax8ex0xax84x0xkx7qx0xux7gx0xx42x0x3ox42x0x3ox4cx",
+                  "respKey":""
                },
-               "taskId": 73052314114,
-               "error": False,
-               "errorBody": None
+               "cost":"0.00299",
+               "ip":"1.2.3.4",
+               "createTime":1692863536,
+               "endTime":1692863556,
+               "solveCount":1,
+               "taskId":75190409731
             }
 
             >>> CapyPuzzle(rucaptcha_key="aa9011f31111181111168611f1151122",
-            ...             captchakey="AVATAR_Cme4hZLjuZRMYC3uh14C52D3uNms5w",
-            ...             pageurl="https://www.capy.me/account/register/",
+            ...             websiteKey="PUZZLE_Cme4hZLjuZRMYC3uh14C52D3uNms5w",
+            ...             websiteURL="https://www.capy.me/account/register/",
             ...             method=CapyPuzzleEnm.CapyTaskProxyless.value,
             ...             api_server="https://jp.api.capy.me/",
             ...             version="avatar",
             ...             ).captcha_handler()
             {
-               "captchaSolve": {
-                  "captchakey": "AVATART_C...w",
-                  "challengekey": "Uf....It",
-                  "answer": "26x...x9mx"
+               "errorId":0,
+               "status":"ready",
+               "solution":{
+                  "captchakey":"PUZZLE_Abc1dEFghIJKLM2no34P56q7rStu8v",
+                  "challengekey":"qHAPtn68KTnXFM8VQ3mtYRtmy3cSKuHJ",
+                  "answer":"0xax8ex0xax84x0xkx7qx0xux7gx0xx42x0x3ox42x0x3ox4cx",
+                  "respKey":""
                },
-               "taskId": 73052314114,
-               "error": False,
-               "errorBody": None
+               "cost":"0.00299",
+               "ip":"1.2.3.4",
+               "createTime":1692863536,
+               "endTime":1692863556,
+               "solveCount":1,
+               "taskId":75190409731
             }
 
             >>> CapyPuzzle(rucaptcha_key="aa9011f31111181111168611f1151122",
-            ...             captchakey="PUZZLE_Cme4hZLjuZRMYC3uh14C52D3uNms5w",
-            ...             pageurl="https://www.capy.me/account/register/",
+            ...             websiteKey="PUZZLE_Cme4hZLjuZRMYC3uh14C52D3uNms5w",
+            ...             websiteURL="https://www.capy.me/account/register/",
             ...             method="CapyTaskProxyless",
             ...             api_server="https://jp.api.capy.me/",
             ...             version="puzzle",
             ...             ).captcha_handler()
             {
-               "captchaSolve": {
-                  "captchakey": "PUZZLE_C...w",
-                  "challengekey": "Uf....It",
-                  "answer": "26x...x9mx"
+               "errorId":0,
+               "status":"ready",
+               "solution":{
+                  "captchakey":"PUZZLE_Abc1dEFghIJKLM2no34P56q7rStu8v",
+                  "challengekey":"qHAPtn68KTnXFM8VQ3mtYRtmy3cSKuHJ",
+                  "answer":"0xax8ex0xax84x0xkx7qx0xux7gx0xx42x0x3ox42x0x3ox4cx",
+                  "respKey":""
                },
-               "taskId": 73052314114,
-               "error": False,
-               "errorBody": None
+               "cost":"0.00299",
+               "ip":"1.2.3.4",
+               "createTime":1692863536,
+               "endTime":1692863556,
+               "solveCount":1,
+               "taskId":75190409731
             }
 
         Returns:
             Dict with full server response
 
         Notes:
-            https://rucaptcha.com/api-rucaptcha#solving_capy
+            https://rucaptcha.com/api-docs/capy-puzzle-captcha
         """
         super().__init__(method=method, *args, **kwargs)
 
-        self.create_task_payload["task"].update({"captchakey": captchakey, "pageurl": pageurl})
+        self.create_task_payload["task"].update({"websiteURL": websiteURL, "websiteKey": websiteKey})
 
         # check user params
         if method not in CapyPuzzleEnm.list_values():
@@ -90,43 +108,6 @@ class CapyPuzzle(BaseCaptcha):
 
         Args:
             kwargs: additional params for `requests` library
-
-        Examples:
-            >>> CapyPuzzle(rucaptcha_key="aa9011f31111181111168611f1151122",
-            ...             captchakey="PUZZLE_Cme4hZLjuZRMYC3uh14C52D3uNms5w",
-            ...             pageurl="https://www.capy.me/account/register/",
-            ...             method=CapyPuzzleEnm.CapyTaskProxyless.value,
-            ...             api_server="https://jp.api.capy.me/",
-            ...             version="puzzle",
-            ...             ).captcha_handler()
-            {
-               "captchaSolve": {
-                  "captchakey": "PUZZLE_C...w",
-                  "challengekey": "Uf....It",
-                  "answer": "26x...x9mx"
-               },
-               "taskId": 73052314114,
-               "error": False,
-               "errorBody": None
-            }
-
-            >>> CapyPuzzle(rucaptcha_key="aa9011f31111181111168611f1151122",
-            ...             captchakey="PUZZLE_Cme4hZLjuZRMYC3uh14C52D3uNms5w",
-            ...             pageurl="https://www.capy.me/account/register/",
-            ...             method=CapyPuzzleEnm.CapyTaskProxyless.value,
-            ...             api_server="https://jp.api.capy.me/",
-            ...             version="avatar",
-            ...             ).captcha_handler()
-            {
-               "captchaSolve": {
-                  "captchakey": "AVATART_C...w",
-                  "challengekey": "Uf....It",
-                  "answer": "26x...x9mx"
-               },
-               "taskId": 73052314114,
-               "error": False,
-               "errorBody": None
-            }
 
         Returns:
             Dict with full server response
@@ -140,43 +121,6 @@ class CapyPuzzle(BaseCaptcha):
     async def aio_captcha_handler(self):
         """
         Async solving method
-
-        Examples:
-            >>> await CapyPuzzle(rucaptcha_key="aa9011f31111181111168611f1151122",
-            ...             captchakey="PUZZLE_Cme4hZLjuZRMYC3uh14C52D3uNms5w",
-            ...             pageurl="https://www.capy.me/account/register/",
-            ...             method=CapyPuzzleEnm.CapyTaskProxyless.value,
-            ...             api_server="https://jp.api.capy.me/",
-            ...             version="puzzle",
-            ...             ).aio_captcha_handler()
-            {
-               "captchaSolve": {
-                  "captchakey": "PUZZLE_C...w",
-                  "challengekey": "Uf....It",
-                  "answer": "26x...x9mx"
-               },
-               "taskId": 73052314114,
-               "error": False,
-               "errorBody": None
-            }
-
-            >>> await CapyPuzzle(rucaptcha_key="aa9011f31111181111168611f1151122",
-            ...             captchakey="PUZZLE_Cme4hZLjuZRMYC3uh14C52D3uNms5w",
-            ...             pageurl="https://www.capy.me/account/register/",
-            ...             method=CapyPuzzleEnm.CapyTaskProxyless.value,
-            ...             api_server="https://jp.api.capy.me/",
-            ...             version="avatar",
-            ...             ).aio_captcha_handler()
-            {
-               "captchaSolve": {
-                  "captchakey": "AVATART_C...w",
-                  "challengekey": "Uf....It",
-                  "answer": "26x...x9mx"
-               },
-               "taskId": 73052314114,
-               "error": False,
-               "errorBody": None
-            }
 
         Returns:
             Dict with full server response
