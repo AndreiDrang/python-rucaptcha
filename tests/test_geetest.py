839fa6949b9f6b889e2cd9b6e58bfc4ed23870f9
Andrei <drang.andray@gmail.com>
2023-12-06 22:50:45 +0300

    Update test_geetest.py

--- tests/test_geetest.py
+++ tests/test_geetest.py
@@ -3,7 +3,6 @@ import pytest
 from tests.conftest import BaseTest
 from python_rucaptcha.gee_test import GeeTest
 from python_rucaptcha.core.enums import GeetestEnm
-from python_rucaptcha.core.serializer import GetTaskResultResponseSer
 
 
 class TestGeeTestBase(BaseTest):
@@ -14,11 +13,13 @@ class TestGeeTestBase(BaseTest):
 
 
 class TestGeeTestCore(TestGeeTestBase):
-    pageurl = "https://www.geetest.com/en/demo"
-    challenge = "1ad03db8aff920037fb8117827eab171"
-    gt = "022397c99c9f646f6477822485f30404"
-    api_server = "api.geetest.com"
-
+    kwargs_params = {
+        "geetestApiServerSubdomain": "api-na.geetest.com",
+        "userAgent": "Some specific user agent",
+        "proxyType": "socks5",
+        "proxyAddress": BaseTest.proxyAddress,
+        "proxyPort": BaseTest.proxyPort,
+    }
     """
     Success tests
     """
@@ -26,6 +27,30 @@ class TestGeeTestCore(TestGeeTestBase):
     def test_methods_exists(self):
         assert "captcha_handler" in GeeTest.__dict__.keys()
         assert "aio_captcha_handler" in GeeTest.__dict__.keys()
+
+    @pytest.mark.parametrize("method", GeetestEnm.GeeTestTaskProxyless.list_values())
+    def test_args(self, method: str):
+        instance = GeeTest(
+            rucaptcha_key=self.RUCAPTCHA_KEY,
+            websiteURL=self.pageurl,
+            method=method,
+            gt=self.gt,
+        )
+        assert instance.create_task_payload["clientKey"] == self.RUCAPTCHA_KEY
+        assert instance.create_task_payload["task"]["type"] == method
+        assert instance.create_task_payload["task"]["websiteURL"] == self.pageurl
+        assert instance.create_task_payload["task"]["gt"] == self.gt
+
+    def test_kwargs(self):
+        instance = GeeTest(
+            rucaptcha_key=self.RUCAPTCHA_KEY,
+            websiteURL=self.pageurl,
+            method=GeetestEnm.GeeTestTaskProxyless.value,
+            gt=self.gt,
+            **self.kwargs_params,
+        )
+        assert set(self.kwargs_params.keys()).issubset(set(instance.create_task_payload["task"].keys()))
+        assert set(self.kwargs_params.values()).issubset(set(instance.create_task_payload["task"].values()))
 
 
 class TestGeeTest(TestGeeTestBase):
@@ -36,84 +61,58 @@ class TestGeeTest(TestGeeTestBase):
     def test_basic_data(self):
         instance = GeeTest(
             rucaptcha_key=self.RUCAPTCHA_KEY,
-            gt=self.gt,
-            method=GeetestEnm.GeeTestTaskProxyless.value,
             websiteURL=self.pageurl,
-            api_server=self.api_server,
-            new_captcha=1,
+            method=GeetestEnm.GeeTestTaskProxyless.value,
+            gt=self.gt,
         )
-
-        assert instance.create_task_payload["method"] == GeetestEnm.GeeTestTaskProxyless.value
-        assert instance.create_task_payload["pageurl"] == self.pageurl
-        assert instance.create_task_payload["api_server"] == self.api_server
-        assert instance.create_task_payload["gt"] == self.gt
-        assert instance.create_task_payload["new_captcha"] == 1
 
         result = instance.captcha_handler(challenge=self.challenge)
 
         assert isinstance(result, dict) is True
-        assert result["error"] is True
-        assert isinstance(result["taskId"], int) is True
-        assert isinstance(result["errorBody"], str) is True
-        assert result["errorBody"] == "ERROR_CAPTCHA_UNSOLVABLE"
-        assert result.keys() == GetTaskResultResponseSer().to_dict().keys()
+        if not result["errorId"]:
+            assert result["status"] == "ready"
+            assert isinstance(result["solution"]["text"], str) is True
+            assert isinstance(result["taskId"], int) is True
+        else:
+            assert result["errorId"] in (1, 12)
+            assert result["errorCode"] == "ERROR_CAPTCHA_UNSOLVABLE"
 
-    @pytest.mark.asyncio
     async def test_aio_basic_data(self):
         instance = GeeTest(
             rucaptcha_key=self.RUCAPTCHA_KEY,
-            gt=self.gt,
-            method=GeetestEnm.GeeTestTaskProxyless.value,
             websiteURL=self.pageurl,
-            api_server=self.api_server,
-            new_captcha=1,
+            method=GeetestEnm.GeeTestTaskProxyless.value,
+            gt=self.gt,
         )
-
-        assert instance.create_task_payload["method"] == GeetestEnm.GeeTestTaskProxyless.value
-        assert instance.create_task_payload["pageurl"] == self.pageurl
-        assert instance.create_task_payload["api_server"] == self.api_server
-        assert instance.create_task_payload["gt"] == self.gt
-        assert instance.create_task_payload["new_captcha"] == 1
 
         result = await instance.aio_captcha_handler(challenge=self.challenge)
 
         assert isinstance(result, dict) is True
-        assert result["error"] is True
-        assert isinstance(result["taskId"], int) is True
-        assert isinstance(result["errorBody"], str) is True
-        assert result["errorBody"] == "ERROR_CAPTCHA_UNSOLVABLE"
-        assert result.keys() == GetTaskResultResponseSer().to_dict().keys()
+        if not result["errorId"]:
+            assert result["status"] == "ready"
+            assert isinstance(result["solution"]["text"], str) is True
+            assert isinstance(result["taskId"], int) is True
+        else:
+            assert result["errorId"] in (1, 12)
+            assert result["errorCode"] == "ERROR_CAPTCHA_UNSOLVABLE"
 
     def test_context_basic_data(self):
         with GeeTest(
             rucaptcha_key=self.RUCAPTCHA_KEY,
-            gt=self.gt,
-            method=GeetestEnm.GeeTestTaskProxyless.value,
             websiteURL=self.pageurl,
-            api_server=self.api_server,
-            new_captcha=1,
+            method=GeetestEnm.GeeTestTaskProxyless.value,
+            gt=self.gt,
         ) as instance:
-            assert instance.create_task_payload["method"] == GeetestEnm.GeeTestTaskProxyless.value
-            assert instance.create_task_payload["pageurl"] == self.pageurl
-            assert instance.create_task_payload["api_server"] == self.api_server
-            assert instance.create_task_payload["gt"] == self.gt
-            assert instance.create_task_payload["new_captcha"] == 1
+            assert instance.captcha_handler(challenge=self.challenge)
 
-    @pytest.mark.asyncio
     async def test_context_aio_basic_data(self):
         async with GeeTest(
             rucaptcha_key=self.RUCAPTCHA_KEY,
-            gt=self.gt,
-            method=GeetestEnm.GeeTestTaskProxyless.value,
             websiteURL=self.pageurl,
-            api_server=self.api_server,
-            new_captcha=1,
+            method=GeetestEnm.GeeTestTaskProxyless.value,
+            gt=self.gt,
         ) as instance:
-            assert instance.create_task_payload["method"] == GeetestEnm.GeeTestTaskProxyless.value
-            assert instance.create_task_payload["pageurl"] == self.pageurl
-            assert instance.create_task_payload["api_server"] == self.api_server
-            assert instance.create_task_payload["gt"] == self.gt
-            assert instance.create_task_payload["new_captcha"] == 1
+            assert await instance.aio_captcha_handler(challenge=self.challenge)
 
     """
     Fail tests
@@ -130,7 +129,7 @@ class TestGeeTest(TestGeeTestBase):
             )
 
     def test_wrong_method_arg(self):
-        with pytest.raises(ValueError):
+        with pytest.raises(TypeError):
             GeeTest(
                 rucaptcha_key=self.RUCAPTCHA_KEY,
                 websiteURL=self.pageurl,
@@ -148,7 +147,6 @@ class TestGeeTest(TestGeeTestBase):
                 method=GeetestEnm.GeeTestTaskProxyless.value,
             ).captcha_handler()
 
-    @pytest.mark.asyncio
     async def test_empty_challenge_aio(self):
         with pytest.raises(ValueError):
             await GeeTest(
@@ -165,83 +163,69 @@ class TestGeeTestV4(TestGeeTestBase):
         instance = GeeTest(
             rucaptcha_key=self.RUCAPTCHA_KEY,
             gt=self.gt,
-            method=GeetestEnm.GeeTestTaskProxyless.value,
             websiteURL=self.pageurl,
             api_server=self.api_server,
-            new_captcha=1,
+            method=GeetestEnm.GeeTestTaskProxyless.value,
+            version=4,
+            initParameters={"captcha_id": "e392e1d7fd421dc63325744d5a2b9c73"},
         )
-
-        assert instance.create_task_payload["method"] == GeetestEnm.GeeTestTaskProxyless.value
-        assert instance.create_task_payload["pageurl"] == self.pageurl
-        assert instance.create_task_payload["api_server"] == self.api_server
-        assert instance.create_task_payload["gt"] == self.gt
-        assert instance.create_task_payload["new_captcha"] == 1
-
         result = instance.captcha_handler(challenge=self.challenge)
 
         assert isinstance(result, dict) is True
-        assert result["error"] is True
-        assert isinstance(result["taskId"], int) is True
-        assert isinstance(result["errorBody"], str) is True
-        assert result["errorBody"] == "ERROR_CAPTCHA_UNSOLVABLE"
-        assert result.keys() == GetTaskResultResponseSer().to_dict().keys()
+        if not result["errorId"]:
+            assert result["status"] == "ready"
+            assert isinstance(result["solution"]["text"], str) is True
+            assert isinstance(result["taskId"], int) is True
+        else:
+            assert result["errorId"] in (1, 12)
+            assert result["errorCode"] == "ERROR_CAPTCHA_UNSOLVABLE"
 
     @pytest.mark.asyncio
     async def test_aio_basic_data(self):
         instance = GeeTest(
             rucaptcha_key=self.RUCAPTCHA_KEY,
             gt=self.gt,
-            method=GeetestEnm.GeeTestTaskProxyless.value,
             websiteURL=self.pageurl,
             api_server=self.api_server,
-            new_captcha=1,
+            method=GeetestEnm.GeeTestTaskProxyless.value,
+            version=4,
+            initParameters={"captcha_id": "e392e1d7fd421dc63325744d5a2b9c73"},
         )
-
-        assert instance.create_task_payload["method"] == GeetestEnm.GeeTestTaskProxyless.value
-        assert instance.create_task_payload["pageurl"] == self.pageurl
-        assert instance.create_task_payload["api_server"] == self.api_server
-        assert instance.create_task_payload["gt"] == self.gt
-        assert instance.create_task_payload["new_captcha"] == 1
-
         result = await instance.aio_captcha_handler(challenge=self.challenge)
 
         assert isinstance(result, dict) is True
-        assert result["error"] is True
-        assert isinstance(result["taskId"], int) is True
-        assert isinstance(result["errorBody"], str) is True
-        assert result["errorBody"] == "ERROR_CAPTCHA_UNSOLVABLE"
-        assert result.keys() == GetTaskResultResponseSer().to_dict().keys()
+        if not result["errorId"]:
+            assert result["status"] == "ready"
+            assert isinstance(result["solution"]["text"], str) is True
+            assert isinstance(result["taskId"], int) is True
+        else:
+            assert result["errorId"] in (1, 12)
+            assert result["errorCode"] == "ERROR_CAPTCHA_UNSOLVABLE"
 
     def test_context_basic_data(self):
         with GeeTest(
             rucaptcha_key=self.RUCAPTCHA_KEY,
             gt=self.gt,
-            method=GeetestEnm.GeeTestTaskProxyless.value,
             websiteURL=self.pageurl,
             api_server=self.api_server,
-            new_captcha=1,
+            method=GeetestEnm.GeeTestTaskProxyless.value,
+            version=4,
+            initParameters={"captcha_id": "e392e1d7fd421dc63325744d5a2b9c73"},
         ) as instance:
-            assert instance.create_task_payload["method"] == GeetestEnm.GeeTestTaskProxyless.value
-            assert instance.create_task_payload["pageurl"] == self.pageurl
-            assert instance.create_task_payload["api_server"] == self.api_server
-            assert instance.create_task_payload["gt"] == self.gt
-            assert instance.create_task_payload["new_captcha"] == 1
+            assert instance.captcha_handler(challenge=self.challenge)
 
     @pytest.mark.asyncio
     async def test_context_aio_basic_data(self):
         async with GeeTest(
             rucaptcha_key=self.RUCAPTCHA_KEY,
             gt=self.gt,
-            method=GeetestEnm.GeeTestTaskProxyless.value,
             websiteURL=self.pageurl,
             api_server=self.api_server,
-            new_captcha=1,
+            method=GeetestEnm.GeeTestTaskProxyless.value,
+            version=4,
+            initParameters={"captcha_id": "e392e1d7fd421dc63325744d5a2b9c73"},
         ) as instance:
-            assert instance.create_task_payload["method"] == GeetestEnm.GeeTestTaskProxyless.value
-            assert instance.create_task_payload["pageurl"] == self.pageurl
-            assert instance.create_task_payload["api_server"] == self.api_server
-            assert instance.create_task_payload["gt"] == self.gt
-            assert instance.create_task_payload["new_captcha"] == 1
+            assert await instance.aio_captcha_handler(challenge=self.challenge)
 
     """
     Fail tests
@@ -251,17 +235,21 @@ class TestGeeTestV4(TestGeeTestBase):
         with pytest.raises(ValueError):
             GeeTest(
                 rucaptcha_key=self.RUCAPTCHA_KEY,
-                captcha_id=self.get_random_string(length=5),
+                gt=self.gt,
                 websiteURL=self.pageurl,
                 api_server=self.api_server,
                 method=self.get_random_string(length=5),
+                version=4,
+                initParameters={"captcha_id": "e392e1d7fd421dc63325744d5a2b9c73"},
             )
 
     def test_wrong_method_arg(self):
-        with pytest.raises(ValueError):
+        with pytest.raises(TypeError):
             GeeTest(
                 rucaptcha_key=self.RUCAPTCHA_KEY,
                 websiteURL=self.pageurl,
                 api_server=self.api_server,
                 method=GeetestEnm.GeeTestTaskProxyless.value,
+                version=4,
+                initParameters={"captcha_id": "e392e1d7fd421dc63325744d5a2b9c73"},
             )
