d145c57bb5a61578f56c182048720242869e9c4a
Andrei <drang.andray@gmail.com>
2023-12-06 20:33:13 +0300

    Update audio_captcha.py

--- src/python_rucaptcha/audio_captcha.py
+++ src/python_rucaptcha/audio_captcha.py
@@ -4,6 +4,7 @@ from typing import Optional
 
 from .core.base import BaseCaptcha
 from .core.enums import SaveFormatsEnm, AudioCaptchaEnm
+from .core.serializer import GetTaskResultResponseSer
 
 
 class AudioCaptcha(BaseCaptcha):
@@ -77,12 +78,13 @@ class AudioCaptcha(BaseCaptcha):
             https://rucaptcha.com/api-rucaptcha#audio
         """
 
-        super().__init__(method=AudioCaptchaEnm.AUDIO.value, *args, **kwargs)
+        super().__init__(method=AudioCaptchaEnm.AudioTask.value, *args, **kwargs)
         self.save_format = save_format
         self.audio_clearing = audio_clearing
         self.audio_path = audio_path
+        self.result = GetTaskResultResponseSer()
 
-        self.post_payload.update({"lang": lang})
+        self.create_task_payload["task"].update({"lang": lang})
 
     def captcha_handler(
         self,
@@ -100,27 +102,6 @@ class AudioCaptcha(BaseCaptcha):
             captcha_base64: Captcha file BASE64 info
             kwargs: additional params for `requests` library
 
-        Examples:
-            >>> AudioCaptcha(rucaptcha_key="aa9011f31111181111168611f1151122",
-            ...             lang='en'
-            ...             ).captcha_handler(captcha_file='examples/mediacaptcha_audio/recaptcha_55914.mp3')
-            {
-                'captchaSolve': 'five five nine one four',
-                'taskId': 73243152973,
-                'error': False,
-                'errorBody': None
-            }
-
-            >>> AudioCaptcha(rucaptcha_key="aa9011f31111181111168611f1151122",
-            ...             lang='en'
-            ...             ).captcha_handler(captcha_link='http://some/link/address/recaptcha_55914.mp3')
-            {
-                'captchaSolve': 'five five nine one four',
-                'taskId': 73243152973,
-                'error': False,
-                'errorBody': None
-            }
-
         Returns:
             Dict with full server response
 
@@ -129,10 +110,12 @@ class AudioCaptcha(BaseCaptcha):
         """
         # if a local file link is passed
         if captcha_file:
-            self.post_payload.update({"body": base64.b64encode(self._local_file_captcha(captcha_file)).decode("utf-8")})
+            self.create_task_payload["task"].update(
+                {"body": base64.b64encode(self._local_file_captcha(captcha_file)).decode("utf-8")}
+            )
         # if the file is transferred in base64 encoding
         elif captcha_base64:
-            self.post_payload.update({"body": base64.b64encode(captcha_base64).decode("utf-8")})
+            self.create_task_payload["task"].update({"body": base64.b64encode(captcha_base64).decode("utf-8")})
         # if a URL is passed
         elif captcha_link:
             try:
@@ -145,7 +128,7 @@ class AudioCaptcha(BaseCaptcha):
             # according to the value of the passed parameter, select the function to save the file
             if self.save_format == SaveFormatsEnm.CONST.value:
                 self._file_const_saver(content, self.audio_path, file_extension="mp3")
-            self.post_payload.update({"body": base64.b64encode(content).decode("utf-8")})
+            self.create_task_payload["task"].update({"body": base64.b64encode(content).decode("utf-8")})
 
         else:
             # if none of the parameters are passed
@@ -171,26 +154,6 @@ class AudioCaptcha(BaseCaptcha):
             captcha_base64: Captcha file BASE64
             kwargs: additional params for `aiohttp` library
 
-        Examples:
-            >>> await AudioCaptcha(rucaptcha_key="aa9011f31111181111168611f1151122",
-            ...             lang='en'
-            ...             ).aio_captcha_handler(captcha_file='examples/mediacaptcha_audio/recaptcha_55914.mp3')
-            {
-                'captchaSolve': 'five five nine one four',
-                'taskId': 73243152973,
-                'error': False,
-                'errorBody': None
-            }
-            >>> await AudioCaptcha(rucaptcha_key="aa9011f31111181111168611f1151122",
-            ...             lang='en'
-            ...             ).aio_captcha_handler(captcha_link='http://some/link/address/recaptcha_55914.mp3')
-            {
-                'captchaSolve': 'five five nine one four',
-                'taskId': 73243152973,
-                'error': False,
-                'errorBody': None
-            }
-
         Returns:
             Dict with full server response
 
@@ -199,10 +162,12 @@ class AudioCaptcha(BaseCaptcha):
         """
         # if a local file link is passed
         if captcha_file:
-            self.post_payload.update({"body": base64.b64encode(self._local_file_captcha(captcha_file)).decode("utf-8")})
+            self.create_task_payload["task"].update(
+                {"body": base64.b64encode(self._local_file_captcha(captcha_file)).decode("utf-8")}
+            )
         # if the file is transferred in base64 encoding
         elif captcha_base64:
-            self.post_payload.update({"body": base64.b64encode(captcha_base64).decode("utf-8")})
+            self.create_task_payload["task"].update({"body": base64.b64encode(captcha_base64).decode("utf-8")})
         # if a URL is passed
         elif captcha_link:
             try:
@@ -215,7 +180,7 @@ class AudioCaptcha(BaseCaptcha):
             # according to the value of the passed parameter, select the function to save the file
             if self.save_format == SaveFormatsEnm.CONST.value:
                 self._file_const_saver(content, self.audio_path, file_extension="mp3")
-            self.post_payload.update({"body": base64.b64encode(content).decode("utf-8")})
+            self.create_task_payload["task"].update({"body": base64.b64encode(content).decode("utf-8")})
 
         else:
             # if none of the parameters are passed
