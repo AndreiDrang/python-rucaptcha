a4630ac7fa09e9572093cf2adc3e4f100602c26e
Andrei <drang.andray@gmail.com>
2023-12-06 18:44:13 +0300

    Update control.py

--- src/python_rucaptcha/control.py
+++ src/python_rucaptcha/control.py
@@ -16,13 +16,38 @@ class Control(BaseCaptcha):
             action: Control action type
 
         Examples:
-            >>> Control(rucaptcha_key="aa9.....",
-            ...            action=ControlEnm.getBalance.value).report()
+            >>> Control(rucaptcha_key="aa9.....").getBalance()
             {
-                'captchaSolve': '1',
-                'taskId': None,
-                'error': False,
-                'errorBody': None
+                'balance': 1593.4479
+            }
+
+            >>> await Control(rucaptcha_key="aa9.....").aio_getBalance()
+            {
+                'balance': 1593.4479
+            }
+
+            >>> Control(rucaptcha_key="aa9.....").reportCorrect(id=75188571838)
+            {
+                'errorId': 0,
+                'status': 'success'
+            }
+
+            >>> await Control(rucaptcha_key="aa9.....").aio_reportCorrect(id=75188571838)
+            {
+                'errorId': 0,
+                'status': 'success'
+            }
+
+            >>> Control(rucaptcha_key="aa9.....").reportIncorrect(id=75188571838)
+            {
+                'errorId': 0,
+                'status': 'success'
+            }
+
+            >>> await Control(rucaptcha_key="aa9.....").aio_reportIncorrect(id=75188571838)
+            {
+                'errorId': 0,
+                'status': 'success'
             }
 
         Returns:
@@ -36,104 +61,84 @@ class Control(BaseCaptcha):
 
         super().__init__(method=ControlEnm.control, *args, **kwargs)
 
-    def report(self, id: str) -> dict:
+    def reportCorrect(self, id: int) -> dict:
         """
-        Captcha results report
+        reportCorrect method
 
         Args:
             id: Captcha task ID
-
-        Examples:
-            >>> Control(rucaptcha_key="aa9011f31111181111168611f1151122",
-            ...            action=ControlEnm.GET.value).report(id="73043727671")
-            {
-                'captchaSolve': '1',
-                'taskId': None,
-                'error': False,
-                'errorBody': None
-            }
-
-            >>> Control(rucaptcha_key="aa9011f31111181111168611f1151122",
-            ...             action=ControlEnm.REPORTGOOD.value).report(id="73043727671")
-            {
-                'captchaSolve': 'OK_REPORT_RECORDED',
-                'taskId': None,
-                'error': False,
-                'errorBody': None
-            }
-
-            >>> Control(rucaptcha_key="aa9011f31111181111168611f1151122",
-            ...             action=ControlEnm.REPORTBAD.value).report(id="73043727671")
-            {
-                'captchaSolve': 'OK_REPORT_RECORDED',
-                'taskId': None,
-                'error': False,
-                'errorBody': None
-            }
-
 
         Returns:
             Dict with full server response
 
         Notes:
-            https://rucaptcha.com/api-rucaptcha#complain
+            https://2captcha.com/api-docs/report-correct
         """
-        self.get_payload.update({"id": id})
+        self.get_task_payload.taskId = id
         return get_sync_result(
-            get_payload=self.get_payload,
+            get_payload=self.get_task_payload,
             sleep_time=self.params.sleep_time,
-            url_response=self.params.url_response,
-            result=self.result,
+            url_response=f"https://api.{self.params.service_type}.com/reportCorrect",
         )
 
-    async def aio_report(self, id: str) -> dict:
+    async def aio_reportCorrect(self, id: int) -> dict:
         """
         Captcha results report
 
         Args:
             id: Captcha task ID
 
-        Examples:
-            >>> await Control(rucaptcha_key="aa9011f31111181111168611f1151122",
-            ...                 action=ControlEnm.GET.value).aio_report(id="73043727671")
-            {
-                'captchaSolve': '1',
-                'taskId': None,
-                'error': False,
-                'errorBody': None
-            }
+        Returns:
+            Dict with full server response
 
-            >>> await Control(rucaptcha_key="aa9011f31111181111168611f1151122",
-            ...                 action=ControlEnm.REPORTGOOD.value).aio_report(id="73043727671")
-            {
-                'captchaSolve': 'OK_REPORT_RECORDED',
-                'taskId': None,
-                'error': False,
-                'errorBody': None
-            }
+        Notes:
+            https://2captcha.com/api-docs/report-correct
+        """
+        self.get_task_payload.taskId = id
+        return await get_async_result(
+            get_payload=self.get_task_payload,
+            sleep_time=self.params.sleep_time,
+            url_response=f"https://api.{self.params.service_type}.com/reportCorrect",
+        )
 
-            >>> await Control(rucaptcha_key="aa9011f31111181111168611f1151122",
-            ...                 action=ControlEnm.REPORTBAD.value).aio_report(id="73043727671")
-            {
-                'captchaSolve': 'OK_REPORT_RECORDED',
-                'taskId': None,
-                'error': False,
-                'errorBody': None
-            }
+    def reportIncorrect(self, id: int) -> dict:
+        """
+        reportCorrect method
 
+        Args:
+            id: Captcha task ID
 
         Returns:
             Dict with full server response
 
         Notes:
-            https://rucaptcha.com/api-rucaptcha#complain
+            https://2captcha.com/api-docs/report-incorrect
         """
-        self.get_payload.update({"id": id})
-        return await get_async_result(
-            get_payload=self.get_payload,
+        self.get_task_payload.taskId = id
+        return get_sync_result(
+            get_payload=self.get_task_payload,
             sleep_time=self.params.sleep_time,
-            url_response=self.params.url_response,
-            result=self.result,
+            url_response=f"https://api.{self.params.service_type}.com/reportIncorrect",
+        )
+
+    async def aio_reportIncorrect(self, id: int) -> dict:
+        """
+        Captcha results report
+
+        Args:
+            id: Captcha task ID
+
+        Returns:
+            Dict with full server response
+
+        Notes:
+            https://2captcha.com/api-docs/report-incorrect
+        """
+        self.get_task_payload.taskId = id
+        return await get_async_result(
+            get_payload=self.get_task_payload,
+            sleep_time=self.params.sleep_time,
+            url_response=f"https://api.{self.params.service_type}.com/reportIncorrect",
         )
 
     def getBalance(self) -> dict:
