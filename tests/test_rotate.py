0a8b8850fa29be129acde947df4d6f9d6a1f99a2
Andrei <drang.andray@gmail.com>
2023-12-07 23:59:32 +0300

    Update test_rotate.py

--- tests/test_rotate.py
+++ tests/test_rotate.py
@@ -1,6 +1,7 @@
 import pytest
 
 from tests.conftest import BaseTest
+from python_rucaptcha.core.enums import RotateCaptchaEnm
 from python_rucaptcha.rotate_captcha import RotateCaptcha
 from python_rucaptcha.core.serializer import GetTaskResultResponseSer
 
@@ -8,14 +9,31 @@ from python_rucaptcha.core.serializer import GetTaskResultResponseSer
 class TestRotateCaptcha(BaseTest):
     captcha_file = "src/examples/rotate/rotate_ex.png"
     captcha_url = "https://rucaptcha.com/dist/web/b771cc7c5eb0c1a811fcb91d54e4443a.png"
-
-    """
-    Success tests
-    """
+    kwargs_params = {
+        "angle": 45,
+        "comment": "comment comm entcomm entcomment",
+    }
 
     def test_methods_exists(self):
         assert "captcha_handler" in RotateCaptcha.__dict__.keys()
         assert "aio_captcha_handler" in RotateCaptcha.__dict__.keys()
+
+    def test_args(self):
+        instance = RotateCaptcha(rucaptcha_key=self.RUCAPTCHA_KEY)
+        assert instance.create_task_payload["clientKey"] == self.RUCAPTCHA_KEY
+        assert instance.create_task_payload["task"]["type"] == RotateCaptchaEnm.RotateTask
+
+    def test_kwargs(self):
+        instance = RotateCaptcha(
+            rucaptcha_key=self.RUCAPTCHA_KEY,
+            **self.kwargs_params,
+        )
+        assert set(self.kwargs_params.keys()).issubset(set(instance.create_task_payload["task"].keys()))
+        assert set(self.kwargs_params.values()).issubset(set(instance.create_task_payload["task"].values()))
+
+    """
+    Success tests
+    """
 
     def test_basic_data_link(self):
         instance = RotateCaptcha(rucaptcha_key=self.RUCAPTCHA_KEY)
@@ -23,14 +41,13 @@ class TestRotateCaptcha(BaseTest):
         result = instance.captcha_handler(captcha_link=self.captcha_url)
 
         assert isinstance(result, dict) is True
-        if result["error"] is False:
-            assert result["error"] is False
+        if not result["errorId"]:
+            assert result["status"] == "ready"
+            assert isinstance(result["solution"]["rotate"], int) is True
             assert isinstance(result["taskId"], int) is True
-            assert result["errorBody"] is None
-            assert isinstance(result["captchaSolve"], str) is True
         else:
-            assert result["error"] is True
-            assert result["errorBody"] == "ERROR_CAPTCHA_UNSOLVABLE"
+            assert result["errorId"] in (1, 12)
+            assert result["errorCode"] == "ERROR_CAPTCHA_UNSOLVABLE"
 
         assert result.keys() == GetTaskResultResponseSer().to_dict().keys()
 
@@ -40,14 +57,13 @@ class TestRotateCaptcha(BaseTest):
         result = instance.captcha_handler(captcha_file=self.captcha_file)
 
         assert isinstance(result, dict) is True
-        if result["error"] is False:
-            assert result["error"] is False
+        if not result["errorId"]:
+            assert result["status"] == "ready"
+            assert isinstance(result["solution"]["rotate"], int) is True
             assert isinstance(result["taskId"], int) is True
-            assert result["errorBody"] is None
-            assert isinstance(result["captchaSolve"], str) is True
         else:
-            assert result["error"] is True
-            assert result["errorBody"] == "ERROR_CAPTCHA_UNSOLVABLE"
+            assert result["errorId"] in (1, 12)
+            assert result["errorCode"] == "ERROR_CAPTCHA_UNSOLVABLE"
 
         assert result.keys() == GetTaskResultResponseSer().to_dict().keys()
 
@@ -58,14 +74,13 @@ class TestRotateCaptcha(BaseTest):
             result = instance.captcha_handler(captcha_base64=f.read())
 
         assert isinstance(result, dict) is True
-        if result["error"] is False:
-            assert result["error"] is False
+        if not result["errorId"]:
+            assert result["status"] == "ready"
+            assert isinstance(result["solution"]["rotate"], int) is True
             assert isinstance(result["taskId"], int) is True
-            assert result["errorBody"] is None
-            assert isinstance(result["captchaSolve"], str) is True
         else:
-            assert result["error"] is True
-            assert result["errorBody"] == "ERROR_CAPTCHA_UNSOLVABLE"
+            assert result["errorId"] in (1, 12)
+            assert result["errorCode"] == "ERROR_CAPTCHA_UNSOLVABLE"
 
         assert result.keys() == GetTaskResultResponseSer().to_dict().keys()
 
@@ -76,14 +91,13 @@ class TestRotateCaptcha(BaseTest):
         result = await instance.aio_captcha_handler(captcha_link=self.captcha_url)
 
         assert isinstance(result, dict) is True
-        if result["error"] is False:
-            assert result["error"] is False
+        if not result["errorId"]:
+            assert result["status"] == "ready"
+            assert isinstance(result["solution"]["rotate"], int) is True
             assert isinstance(result["taskId"], int) is True
-            assert result["errorBody"] is None
-            assert isinstance(result["captchaSolve"], str) is True
         else:
-            assert result["error"] is True
-            assert result["errorBody"] == "ERROR_CAPTCHA_UNSOLVABLE"
+            assert result["errorId"] in (1, 12)
+            assert result["errorCode"] == "ERROR_CAPTCHA_UNSOLVABLE"
 
         assert result.keys() == GetTaskResultResponseSer().to_dict().keys()
 
@@ -94,14 +108,13 @@ class TestRotateCaptcha(BaseTest):
         result = await instance.aio_captcha_handler(captcha_file=self.captcha_file)
 
         assert isinstance(result, dict) is True
-        if result["error"] is False:
-            assert result["error"] is False
+        if not result["errorId"]:
+            assert result["status"] == "ready"
+            assert isinstance(result["solution"]["rotate"], int) is True
             assert isinstance(result["taskId"], int) is True
-            assert result["errorBody"] is None
-            assert isinstance(result["captchaSolve"], str) is True
         else:
-            assert result["error"] is True
-            assert result["errorBody"] == "ERROR_CAPTCHA_UNSOLVABLE"
+            assert result["errorId"] in (1, 12)
+            assert result["errorCode"] == "ERROR_CAPTCHA_UNSOLVABLE"
 
         assert result.keys() == GetTaskResultResponseSer().to_dict().keys()
 
@@ -113,14 +126,13 @@ class TestRotateCaptcha(BaseTest):
             result = await instance.aio_captcha_handler(captcha_base64=f.read())
 
         assert isinstance(result, dict) is True
-        if result["error"] is False:
-            assert result["error"] is False
+        if not result["errorId"]:
+            assert result["status"] == "ready"
+            assert isinstance(result["solution"]["rotate"], int) is True
             assert isinstance(result["taskId"], int) is True
-            assert result["errorBody"] is None
-            assert isinstance(result["captchaSolve"], str) is True
         else:
-            assert result["error"] is True
-            assert result["errorBody"] == "ERROR_CAPTCHA_UNSOLVABLE"
+            assert result["errorId"] in (1, 12)
+            assert result["errorCode"] == "ERROR_CAPTCHA_UNSOLVABLE"
 
         assert result.keys() == GetTaskResultResponseSer().to_dict().keys()
 
