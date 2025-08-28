678e3e5b7fafb76f6466ed9ca9a1c51a6d15c7c9
Andrei <drop.all.requests@gmail.com>
2025-08-28 23:22:21 +0300

    Update serializer.py

--- src/python_rucaptcha/core/serializer.py
+++ src/python_rucaptcha/core/serializer.py
@@ -7,7 +7,7 @@ from .config import APP_KEY
 
 
 class MyBaseModel(Struct):
-    def to_dict(self)->dict[str, Any]:
+    def to_dict(self) -> dict[str, Any]:
         return {f: getattr(self, f) for f in self.__struct_fields__}
 
 
@@ -24,13 +24,13 @@ class CreateTaskBaseSer(MyBaseModel):
     clientKey: str
     task: TaskSer = {}
     languagePool: str = "en"
-    callbackUrl: str|None = None
+    callbackUrl: str | None = None
     soft_id: Literal[APP_KEY] = APP_KEY
 
 
 class GetTaskResultRequestSer(MyBaseModel):
     clientKey: str
-    taskId: int|None = None
+    taskId: int | None = None
 
 
 class CaptchaOptionsSer(MyBaseModel):
@@ -59,16 +59,16 @@ HTTP API Response
 
 class GetTaskResultResponseSer(MyBaseModel):
     status: str = "ready"
-    solution: dict[str, str]|None = None
+    solution: dict[str, str] | None = None
     cost: float = 0.0
-    ip: str|None = None
-    createTime: int|None = None
-    endTime: int|None = None
-    solveCount: int|None = None
-    taskId: int|None = None
+    ip: str | None = None
+    createTime: int | None = None
+    endTime: int | None = None
+    solveCount: int | None = None
+    taskId: int | None = None
     # control method params
-    balance: float|None = None
+    balance: float | None = None
     # error info
     errorId: int = 0
-    errorCode: str|None = None
-    errorDescription: str|None = None
+    errorCode: str | None = None
+    errorDescription: str | None = None
